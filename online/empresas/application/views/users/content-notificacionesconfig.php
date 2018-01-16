f970a82c714ff5bdb4030f9f5610b65aee29e5cd
lvillamizar <lvillamizar@novopayment.com>
2018-01-16 15:35:09 -0400

    Agrega color azul corporativo, ajusta color a parrafo

--- online/empresas/application/views/users/content-notificacionesconfig.php
+++ online/empresas/application/views/users/content-notificacionesconfig.php
@@ -11,7 +11,8 @@
 		 		src="<?php echo $urlCdn."media/img/loading.gif"?>"/>
 	</div>
 	<h1><?php echo lang('NOTI_TITLE_CONFIG'); ?></h1>
-		<p>En esta sección Ud. puede agregar correos electrónicos para notificaciones de sus productos y servicios</p>
+		<p class="corporateBlue">
+			En esta sección Ud. puede agregar correos electrónicos para notificaciones de sus productos y servicios</p>
 		<select class="selectorNotificacion" id="listaEmpresas" name="batch"  onchange="selectorEmpresa()">
 			<?php
 				if(array_key_exists('ERROR', $listaEmpr[0])){
