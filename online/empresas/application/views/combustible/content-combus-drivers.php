533bde890e589866aaa130ffa3094b5aed7f9463
Edinson Cabello <ecabello@novopayment.com>
2018-04-26 10:56:25 -0400

    Implementa descarga del archivo excel de conductores

--- online/empresas/application/views/combustible/content-combus-drivers.php
+++ online/empresas/application/views/combustible/content-combus-drivers.php
@@ -28,21 +28,22 @@
         <span aria-hidden="true" class="icon icon-list" data-icon="&#xe020;"></span>
         <?= lang("BREADCRUMB_DRIVERS"); ?>
     </div>
-    <div class="container-body">
+    <div id="table-drivers" class="container-body">
         <div id='loading' style='text-align:center'><?php echo insert_image_cdn("loading.gif"); ?></div>
         <table id="novo-table" class="hover cell-border" width="620px"></table>
     </div>
-    <div class="contanier-footer">
-        <button id="add" disabled><?= lang("DRIVER_ADD"); ?></button>
+    <div id="footer-filter" class="contanier-footer">
+        <button id="add" disabled><?php echo lang("DRIVER_ADD"); ?></button>
     </div>
 </div>
-<form id='formulario' method='post'></form>
+
+<form id='formularioDriver' method='post'></form>
+<input type="hidden" id="logUrl" value="<?php echo  base_url().$pais; ?>">
 
 <div id="msg-system" style='display:none'>
     <div id="msg-info" class="comb-content"></div>
     <div id="actions" class="comb-content actions-buttons">
         <button id="close-info" class="buttons-action"></button>
-        <button id="send-info" func="search" class="buttons-action"></button>
+        <button id="send-info" class="buttons-action"></button>
     </div>
-    <div id="msg"></div>
 </div>
