54eb41da95fdfe12c46fe2bf8d0ded98a2f0a78d
Abraham Navarro <anavarro@novopayment.com>
2018-05-17 10:20:30 -0400

    Incorpora la ruta y la seccion tarifas.

---
+++ online/empresas/application/views/footerpages/tarifas.php
@@ -0,0 +1,15 @@
+<?php
+	$urlCdn = get_cdn();
+?>
+<div id="content-beneficios">
+	<h1>Tarifas</h1>
+
+	<div class="content-beneficios">
+
+		<div class="content-tarifas">
+			<img src="<?php echo $urlCdn."/media/img/tarifas.png"?>" alt="">
+		</div>
+
+	</div>
+
+</div>
