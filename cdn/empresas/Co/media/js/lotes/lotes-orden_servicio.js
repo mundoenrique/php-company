87a75b77f8aca5c7f5d4bac87ea484740f0ca52f
Abraham Navarro <anavarro@novopayment.com>
2018-12-28 10:49:37 -0400

    Incorpora ajuste en las variables base url de lotes orden servicio js

--- cdn/empresas/Co/media/js/lotes/lotes-orden_servicio.js
+++ cdn/empresas/Co/media/js/lotes/lotes-orden_servicio.js
@@ -26,10 +26,9 @@ $(function(){
 
 	$('#lotes-general').show();
 
-	var path =window.location.href.split( '/' );
-	var baseURL = path[0]+ "//" +path[2]+'/'+path[3];
-	var isoPais = path[4];
-	var api ="/api/v1/";
+	var baseURL = $('body').attr('data-app-base');
+	var isoPais = $('body').attr('data-country');
+	var api ="api/v1/";
 
 
 	//$("#tabla-datos-general").find(".OSinfo").hide(); // ocultar lotes de os
--- cdn/empresas/Ve/media/js/lotes/lotes-orden_servicio.js
+++ cdn/empresas/Ve/media/js/lotes/lotes-orden_servicio.js
@@ -28,10 +28,9 @@ $(function(){
 
 	$('#lotes-general').show();
 
-	var path =window.location.href.split( '/' );
-	var baseURL = path[0]+ "//" +path[2]+'/'+path[3];
-	var isoPais = path[4];
-	var api ="/api/v1/";
+	var baseURL = $('body').attr('data-app-base');
+	var isoPais = $('body').attr('data-country');
+	var api ="api/v1/";
 
 
 	//$("#tabla-datos-general").find(".OSinfo").hide(); // ocultar lotes de os
@@ -140,7 +139,7 @@ $(function(){
 						$(sitio).submit();
 					}
 					if(code === 3) {
-						$(location).attr('href', baseURL + '/' + isoPais + '/login');
+						$(location).attr('href', baseURL + isoPais + '/login');
 					}
 				}
 			}
