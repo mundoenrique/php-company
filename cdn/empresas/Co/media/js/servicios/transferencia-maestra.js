5eb2b165c5210fc2cdf041cd44d53051ada1f5fb
Abraham Navarro <anavarro@novopayment.com>
2019-01-03 11:39:18 -0400

    Incorpora ajuste de base url en pass transferencia maestra js.

--- cdn/empresas/Co/media/js/servicios/transferencia-maestra.js
+++ cdn/empresas/Co/media/js/servicios/transferencia-maestra.js
@@ -1,8 +1,7 @@
 // VARIABLES GLOBALES PARA URL
-var path = window.location.href.split('/');
-var baseURL = path[0] + "//" + path[2] + '/' + path[3];
-var isoPais = path[4];
-var api = "/api/v1/";
+var baseURL = $('body').attr('data-app-base');
+var isoPais = $('body').attr('data-country');
+var api = "api/v1/";
 $(function() {
 
     $('#filtroOS').show();
@@ -1028,7 +1027,7 @@ function notiPagOS (titu, msg, type) {
             OK: function(){
                 $(this).dialog('destroy');
                 if (type == 'close') {
-                    window.location.replace(baseURL+'/'+isoPais+'/logout');
+                    window.location.replace(baseURL + isoPais+'/logout');
                 } else if (type == 'ok') {
                     location.reload(true);
                 }
