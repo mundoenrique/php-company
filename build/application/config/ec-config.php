003444c991fed97cd53af5aa94b2f2e8e6083b31
Abraham Navarro <anavarro@novopayment.com>
2019-01-29 14:57:59 -0400

    Incorpora el archivo Ec config.

---
+++ build/application/config/ec-config.php
@@ -0,0 +1,37 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+//País
+$config['country']='Ec';
+
+/*
+|--------------------------------------------------------------------------
+| Base CDN URL
+|--------------------------------------------------------------------------
+*/
+
+//RUTA BASE PARA ARCHIVOS CDN  Ejemplo: https://cdn.novopayment.dev/empresas/Usd/
+$config['base_url_cdn'] =  BASE_CDN_URL.$config['country'].'/';
+
+//URL PARA CONECTAR POR SFTP A SERVIDOR
+$config['URL_TEMPLOTES'] = 'sftp://172.24.6.102:22/u01/app/data/lotes/temp/'.$config['country'].'/';
+
+//PATCH CARPETA DONDE SE SUBEN LOS LOTES Ejemplo: '/opt/httpd-2.4.4/vhost/cdn/empresas/Usd/bash/''
+$config['FOLDER_UPLOAD_LOTES'] = '/opt/httpd-2.4.4/vhost/cdn/empresas/'.$config['country'].'/'.'bash/';
+
+//PATH CARPETA BASE CDN DEL PAÍS
+$config['CDN'] = BASE_CDN_PATH.$config['country'].'/';
+
+/*
+|--------------------------------------------------------------------------
+| Default Language
+|--------------------------------------------------------------------------
+|
+| This determines which set of language files should be used. Make sure
+| there is an available translation if you intend to use something other
+| than english.
+|
+*/
+$config['language']	= 'usd-spanish';
+
+/* End of file config.php */
+/* Location: ./application/config/config.php */
