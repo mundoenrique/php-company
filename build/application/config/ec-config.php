3954efaae6cdca86473c61b682dd98236977391b
Abraham Navarro <anavarro@novopayment.com>
2019-01-29 16:25:30 -0400

    Incorpora cableado en el ec-config para apuntar a los servicios de colombia.

--- build/application/config/ec-config.php
+++ build/application/config/ec-config.php
@@ -1,7 +1,7 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 //País
-$config['country']='Ec';
+$config['country']='Co';
 
 /*
 |--------------------------------------------------------------------------
@@ -10,16 +10,20 @@ $config['country']='Ec';
 */
 
 //RUTA BASE PARA ARCHIVOS CDN  Ejemplo: https://cdn.novopayment.dev/empresas/Usd/
-$config['base_url_cdn'] =  BASE_CDN_URL.$config['country'].'/';
+//$config['base_url_cdn'] =  BASE_CDN_URL.$config['country'].'/';
+$config['base_url_cdn'] =  BASE_CDN_URL.'Ec/';
 
 //URL PARA CONECTAR POR SFTP A SERVIDOR
-$config['URL_TEMPLOTES'] = 'sftp://172.24.6.102:22/u01/app/data/lotes/temp/'.$config['country'].'/';
+//$config['URL_TEMPLOTES'] = 'sftp://172.24.6.102:22/u01/app/data/lotes/temp/'.$config['country'].'/';
+$config['URL_TEMPLOTES'] = 'sftp://172.24.6.102:22/u01/app/data/lotes/temp/'.'Ec/';
 
 //PATCH CARPETA DONDE SE SUBEN LOS LOTES Ejemplo: '/opt/httpd-2.4.4/vhost/cdn/empresas/Usd/bash/''
-$config['FOLDER_UPLOAD_LOTES'] = '/opt/httpd-2.4.4/vhost/cdn/empresas/'.$config['country'].'/'.'bash/';
+// $config['FOLDER_UPLOAD_LOTES'] = '/opt/httpd-2.4.4/vhost/cdn/empresas/'.$config['country'].'/'.'bash/';
+$config['FOLDER_UPLOAD_LOTES'] = '/opt/httpd-2.4.4/vhost/cdn/empresas/'.'Ec/'.'bash/';
 
 //PATH CARPETA BASE CDN DEL PAÍS
-$config['CDN'] = BASE_CDN_PATH.$config['country'].'/';
+// $config['CDN'] = BASE_CDN_PATH.$config['country'].'/';
+$config['CDN'] = BASE_CDN_PATH.'Ec/';
 
 /*
 |--------------------------------------------------------------------------
