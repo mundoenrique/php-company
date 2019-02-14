4b2a2188dffec09dedf47da05b96e901a92aa9dc
Abraham Navarro <anavarro@novopayment.com>
2019-02-14 10:22:34 -0400

    Incorpora cableado en tool_helper para que carguen estilos de Ec. DEBE SER BORRADO AL FINALIZAR

--- build/application/helpers/tools_helper.php
+++ build/application/helpers/tools_helper.php
@@ -53,6 +53,8 @@
 		{
 			$CI =& get_instance();
 
+			$countryISO="Ec"; //Esta linea debe ser borrada al finalizar el branch feature-docker-ecuador
+
 			switch ($countryISO) {
 				case 'Ve':
 					$CI->config->load('ve-config');
