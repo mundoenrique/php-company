b2bb55899206e972b9891a6fd65532879120e3a4
Enrique Peñaloza <epenaloza@novopayment.com>
2019-03-20 12:02:19 -0500

    Incopora archivos cdn para país Ec-bp (pichincha)

---
+++ build/assets/Ec-bp/media/js/combustible/notiSystem.js
@@ -0,0 +1,59 @@
+function notiSystem (action, title, size = '320px') {
+
+    if (action === 'msg') {
+        $('#msg-system').dialog({
+            title: title,
+            modal: 'true',
+            width: '210px',
+            draggable: false,
+            rezise: false,
+            open: function(event, ui) {
+                $('.ui-dialog-titlebar-close', ui.dialog).hide();
+            }
+        });
+        $('#close-info').on('click', function(e){
+            e.preventDefault();
+            $('#msg-system').dialog('close');
+            $('#msg-info').empty();
+            var finish = $(this).attr('finish');
+            switch (finish) {
+                case 'u':                    
+                    location.reload(true);
+                    break;
+                case 'up':                    
+                    $('form#formulario').empty();
+                    $('form#formulario').append('<input type="hidden" name="data-id" value="' + idflota + '" />');
+                    $('form#formulario').append('<input type="hidden" name="data-name" value="' + nameFlota + '" />');
+                    $('form#formulario').append('<input type="hidden" name="modelo" value="vehicles" />');
+                    $('form#formulario').attr('action', baseURL + '/' + isoPais + '/trayectos/vehiculos');
+                    $('form#formulario').submit();
+                    break;
+                case 'g':
+                    window.location.replace(baseURL + '/' + isoPais + '/trayectos/gruposVehiculos');
+                    break;
+                case 'c':
+                    window.location.replace(baseURL + '/' + isoPais + '/logout');
+                    break;
+            }
+        });
+    } else if (action === 'groups') {
+        $('#add-edit').dialog({
+            modal: "true",
+            width: size,
+            resizable: false,
+            draggable: false,
+            title: title,
+            open: function(event, ui) {
+                $(".ui-dialog-titlebar-close", ui.dialog).hide();
+            }
+        });
+        $('#cancel').click(function(){
+            $('#add-edit').dialog("close");
+            $('#formAddEdit input, #formAddEdit textarea').removeClass('field-error');
+            $('#msg').empty();
+            $('#send-save')
+                .removeClass('withoutChanges')
+                .prop('disabled', false);
+        });
+    }
+}
