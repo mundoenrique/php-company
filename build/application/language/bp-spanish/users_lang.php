c3e976abb0bd29243ba18f686ffc0455db1c41b5
Enrique Peñaloza <epenaloza@novopayment.com>
2019-04-10 12:21:38 -0500

    Incorpora archivos de lenguaje por país

---
+++ build/application/language/bp-spanish/users_lang.php
@@ -0,0 +1,124 @@
+<?php
+$lang['WELCOME_TITLE']='Bienvenido a<br> Conexión Empresas';
+$lang['WELCOME_MESSAGE']='Con este sistema las gestiones de pago de su empresa son más eficientes, seguras y rápidas. Empiece a disfrutar de todas las ventajas que le ofrece Conexión Empresas Online.';
+$lang['WIDGET_LOGIN_TITLE']="Cliente afiliado";
+$lang['WIDGET_ADDCLIENT_TITLE']="Nuevo Cliente";
+
+$lang['LOGIN_INFO']='Ventas e información a empresas';
+$lang['INFO-1']='Teléfono: (57-1) 419-3333';
+$lang['INFO-2']='Ventas: ventas@tebca.com.co / ventas@servitebca.com.co';
+$lang['INFO-3']='Sugerencias: info@tebca.com.co / info@servitebca.com.co';
+
+$lang['FOOTER'] ='Todos los derechos reservados.';
+
+$lang['MSG_BLOQUEAR_USER'] = 'El usuario será bloqueado al próximo intento fallido';
+
+
+//TERMINOS Y CONDICIONES
+$lang['MSG_CONFIR_EXIT'] = 'Para continuar, debe aceptar los términos y condiciones de uso. De lo contrario será dirigido a la página inicial';
+$lang['MSG_NEW_PASS_USER'] = 'Por motivos de seguridad es necesario que cambie su contraseña la primera vez que ingresa a nuestro sistema "Conexión Empresas Online"';
+$lang['MSG_NEW_PASS_CADU'] = 'Clave vencida. Por motivos de seguridad es necesario que cambie su contraseña antes de continuar en nuestro sistema "Conexión Empresas Online"';
+
+//CONFIGURACION
+$lang['CARGANDO'] = 'Cargando...';
+$lang['CONFIGURACION'] = 'Configuración';
+$lang['SUBMENU_EMPRESAS'] = 'Empresas';
+$lang['SUBMENU_SUCURSALES'] = 'Sucursales';
+$lang['SUBMENU_USUARIO'] = 'Usuario';
+$lang['SUBMENU_DESCARGAS'] = 'Descargas';
+$lang['SUBMENU_LOGOUT'] = 'Salir';
+$lang['CONFIG_USER'] = 'Configuración de usuario';
+$lang['CONFIG_EMPR'] = 'Configuración de empresa';
+$lang['CONFIG_SUCU'] = 'Configuración de sucursal';
+$lang['TITLE_MODIFICAR'] = 'Modificar';
+
+
+// TAB USUARIO
+$lang['INFO_USER_NAME'] = 'Nombre: ';
+$lang['INFO_USER_APELLIDO'] = 'Apellido: ';
+$lang['INFO_USER_CARGO'] = 'Cargo: ';
+$lang['INFO_USER_AREA'] = 'Área: ';
+$lang['INFO_USER_EMAIL'] = 'E-mail: ';
+$lang['BOTON_MOD_USER'] = 'Modificar datos de usuario';
+$lang['BOTON_CAMBIO_CLAVE'] = 'Cambiar contraseña';
+
+// TAB EMPRESA
+$lang['EMP_TITLE_CONFIG'] = 'Configuración de la empresa';
+$lang['EMP_CARGANDO'] = 'Cargando...';
+$lang['EMP_IDENTIFICADOR'] = 'Nro. identificador:';
+$lang['EMP_NOMBRE'] = 'Nombre: ';
+$lang['EMP_RAZON_SOCIAL'] = 'Razón social: ';
+$lang['EMP_CONTACTO'] = 'Contacto: ';
+$lang['EMP_DIR_UBICACION'] = 'Direccion: ';
+$lang['EMP_DIR_FACTURACION'] = 'Dirección de facturación: ';
+$lang['TITLE_TELEFONO1'] = 'Teléfono 1: ';
+$lang['TITLE_TELEFONO2'] = 'Teléfono 2: ';
+$lang['TITLE_TELEFONO3'] = 'Teléfono 3: ';
+$lang['EMP_BTN_MODIF'] = 'Modificar datos empresa';
+$lang['EMP_BTN_CONTACT'] = 'Mostrar contactos empresa';
+$lang['EMP_BTN_ADD_CONTACT'] = 'Agregar';
+$lang['EMP_BTN_ELIM_CONTACT'] = 'Eliminar';
+$lang['EMP_BTN_MODIF_CONTACT'] = 'Modificar';
+$lang['EMP_BTN_AGREGAR_CONTACT'] = 'Añadir';
+$lang['EMP_BTN_AGREGAR_CONTACT_EMPRESA'] = 'Agregar contacto';
+$lang['EMP_BTN_LIMPIAR_CONTACT'] = 'Limpiar';
+$lang['EMP_ID'] = 'ID';
+$lang['EMP_CONTACT_TIPO'] = 'Tipo';
+
+// TAB SUCURSAL
+$lang['SUC_TITLE'] = 'Configuración de sucursales';
+$lang['SUC_EMP'] = 'Empresa';
+$lang['SUC_NOMB'] = 'Nombre: ';
+$lang['SUC_ZONA'] = 'Zona: ';
+$lang['SUC_DIRECCION_1'] = 'Dirección 1:';
+$lang['SUC_DIRECCION_2'] = 'Dirección 2: ';
+$lang['SUC_DIRECCION_3'] = 'Dirección 3: ';
+$lang['SUC_PAIS'] = 'País: ';
+$lang['SUC_ESTADO'] = 'Departamento: ';
+$lang['SUC_CIUDAD'] = 'Provincia: ';
+$lang['SUC_TELEFONO'] = 'Teléfono: ';
+$lang['SUC_CONTACTO'] = 'Contacto: ';
+$lang['SUC_AREA'] = 'Código de área: ';
+$lang['SUC_CODIGO'] = 'Cód. de sucursal: ';
+$lang['SUC_BTN_MODIFICAR'] = 'Modificar';
+$lang['SUC_BTN_NEW_SUC'] = 'Nueva sucursal';
+$lang['SUC_BTN_NEW_MASV'] = 'Subir archivo';
+$lang['SUC_TITLE_NEW_SUC'] = 'Gestión de Sucursal';
+$lang['SUC_BTN_UPFILE'] = 'subir archivo de nuevas sucursales';
+
+//TAB DESCARGAS
+$lang['DWL_MANUAL'] = "TV-MUS-002-conexion-empresas-online-V10.pdf";
+
+// PAGINA LOGOUT
+$lang['LOGOUT_TITULO'] = 'Gracias por usar nuestros servicios';
+$lang['LOGOUT_MSG'] = 'Su sesión en Conexión Empresas Online ha caducado';
+$lang['LOGOUT_BTN_BACK'] ='Aceptar';
+
+$lang["TITULO_TERMINOS"]='Condiciones Generales, Términos de Uso y Confidencialidad';
+$lang["TERMINOS"] = '<p><strong>NovoPayment Colombia S.A.S.</strong>, sociedad legalmente constituida y registrada ante la Cámara de Comercio de Bogotá (en adelante  “La Empresa”); a continuación establece las Condiciones Generales y Términos de Uso y Confidencialidad que rigen la plataforma electrónica desarrollada por La Empresa y que ha sido habilitada para que los clientes de las instituciones financieras emisoras (en adelante los “Usuarios”), puedan hacer uso de las distintas funcionalidades que ofrece.</p>
+<p>En consecuencia, los Usuarios que deseen acceder a la referida plataforma, se obligan a leer las presentes Condiciones Generales y Términos de Uso y Confidencialidad cuidadosamente antes de continuar su recorrido, con lo cual se entenderá que están de acuerdo con las mismas y declaran que aceptan cumplir a cabalidad las instrucciones y obligaciones aquí contenidas.</p>
+<p>La plataforma de La Empresa le proporciona a los Usuarios, las herramientas necesarias para cumplir y hacer uso de las distintas soluciones de gestión de efectivo ofrecidas por las entidades emisoras, de manera rápida y con total seguridad. Sin embargo, La Empresa no garantiza, de forma alguna, el servicio ininterrumpido o libre de error de la misma.</p>
+<p>La página web de La Empresa ofrece el enlace con otras páginas web por lo que el acceso a dichas páginas será por cuenta y riesgo de los Usuarios. De igual forma, los Usuarios podrán encontrar anuncios referentes a las promociones de los productos y servicios de La Empresa los cuales se regirán por los contratos que regulan dichos productos y servicios así como, por los términos y condiciones que rijan las respectivas promociones.</p>
+<p>La Empresa declara que toda la información referente a los Usuarios, será tratada conforme a las disposiciones constitucionales, legales y administrativas que garantizan los derechos a la privacidad de las comunicaciones y del acceso a la información personal, según lo dispuesto en la Ley Estatutaria 1581 de 2012, y su respectivo Decreto Reglamentario 1377 de 2013, así como también lo dispuesto en la Constitución Política de Colombia de 1991.</p>
+<p>Por tal motivo, La Empresa se compromete a nunca emitir ni divulgar datos personales sin el expreso consentimiento de los Usuarios a no ser que sea para verificar datos o cumplir con requisitos legales. No obstante, La Empresa, previa autorización por parte de los Usuarios, podrá transferir estos datos en caso de venta de la empresa, siempre respetando y garantizado la privacidad, el honor y la reputación de las personas.</p>
+<p>En tal sentido, los Usuarios entienden que la información contenida, fijada y puesta a disposición del público en la página web de La Empresa son considerados mensajes de datos, según los términos y condiciones que aquí se exponen. Sobre los mensajes de datos se garantizan los derechos a la privacidad de las comunicaciones y del acceso a la información personal.</p>
+<p>La Empresa declara que los mensajes de datos vienen resguardados por dispositivos de seguridad que permiten verificar que no han sido alterados, que han permanecido íntegros durante su generación, comunicación y transmisión, recepción y archivo, y para confirmar la identidad del emisor, con ello se puede evidenciar su origen. Asimismo, estos mensajes de datos son grabados y pueden ser recuperados para ulterior consulta.</p>
+<p>Así mismo, La Empresa declara que las creaciones intelectuales, tales como marcas, slogans, nombres de dominio, diseños, entre otros, tendrán como único titular de los derechos económicos que de aquellos se deriven a La Empresa, quien es la única que los podrá utilizar conforme a la protección y facultades que les otorga en Colombia el sistema jurídico de propiedad intelectual vigente.</p>
+<p>A su vez, queda expresamente entendido que entre La Empresa y los Usuarios de las bases de datos digitales, originales o no, que han sido creadas con ocasión de brindar servicios y productos con información personal, privada y confidencial de los Usuarios le pertenecen a La Empresa quien, <strong>con expresa autorización por parte de los usuarios</strong>, es la única titular de los derechos económicos sobre la misma. No obstante, La Empresa protegerá y respetará los derechos personalísimos que del uso o almacenamiento de dicha información se genere en cabeza de los Usuarios, conforme a lo dispuesto en la ley Estatutaria 1581 de 2012, y su respectivo Decreto Reglamentario 1377 de 2013.</p>
+<p>En tal sentido, La Empresa hará su mejor esfuerzo para el mejor uso de sus plataformas tecnológicas, garantizando el honor y la intimidad personal y familiar de los Usuarios, y así lo entienden estos últimos. No obstante, La Empresa se reserva el derecho de suspender el servicio temporalmente en cualquier momento, sin previa notificación a los Usuarios, siempre y cuando sea por razones de mantenimiento de la plataforma u otras razones de carácter técnico que se consideren necesarias y convenientes. En estos casos, no existirá ningún tipo de responsabilidad y por ende no generará ningún tipo de indemnización por parte de La Empresa a los Usuarios.</p>
+<p>Los Usuarios, de conformidad con las políticas de seguridad y privacidad de la plataforma tecnológica, al momento de su registro en la misma, tendrán la posibilidad de elegir si desean recibir o no mensajes electrónicos comerciales por parte de La Empresa.</p>
+<p>La Empresa podrá emplear a su total discreción todos los medios necesarios a su alcance para retirar de forma inmediata al Usuario que infrinja cualquiera de las condiciones de seguridad y privacidad de la plataforma tecnológica, lo cual incluye que el Usuario haya enviado información falsa que no sea susceptible de verificación. Igualmente, La Empresa tiene el derecho de cambiar, sin previo aviso, la política de seguridad y privacidad, velando en todo momento el cumplimiento de la normatividad vigente.</p>
+<p>En caso de cualquier violación a por parte de los Usuarios de las condiciones de seguridad y privacidad, de los sistemas que utilizan tecnología de la información, de la privacidad de las personas y comunicaciones y de la propiedad intelectual que estén vinculados a la plataforma tecnológica, se estará en incumplimiento de lo establecido por las distintas normas que regulan la materia.</p>
+<p>Los Usuarios, de conformidad con las políticas de seguridad y privacidad de la plataforma tecnológica, al momento de su registro en la misma, tendrán la posibilidad de elegir si desean recibir o no mensajes electrónicos comerciales por parte de La Empresa.</p>';
+
+
+//change password
+$lang['FORGOT_PASS']= "Si ha olvidado o bloqueado su contraseña de acceso a <strong>Conexión Empresas Online</strong>, por favor ingrese los datos que se solicitan a continuación.";
+$lang['USER_USER']= "Usuario";
+$lang['RIF_NIT']= "RUC de la Empresa";
+$lang['MAIL']= "Correo electrónico";
+$lang['INFO_CHANGE_PASS']= "Cambio de contraseña";
+$lang['INFO_REDIRECT']= "Redireccionando...";
+$lang['CHANGE_OK']= "Proceso exitoso";
+$lang['PLACE_HOLDER_NIT'] = "0000000000000";
+$lang['PLACE_HOLDER_MAIL'] = "usuario@ejemplo.com";
---
+++ build/application/language/us-spanish/servicios_lang.php
@@ -0,0 +1,20 @@
+<?php
+$lang['TITULO_TRANSMAESTRA']="Transferencia maestra";
+
+$lang['CRITERIOS_BUSQUEDA'] = 'Criterio de búsqueda';
+$lang['SEL_EMPRESA'] = 'Empresa';
+$lang['SEL_OPTION_EMP'] = 'Seleccione una empresa';
+$lang['NRO_TARJETA'] = 'Número de tarjeta';
+$lang['BUSCAR'] = 'Buscar';
+$lang['RESULTADOS'] = 'Resultados';
+$lang['CARGO'] = 'Cargo';
+$lang['CONSULTA'] = 'Consulta';
+$lang['ABONO'] = 'Abono';
+$lang['PLACEHOLDER_PASS']='Ingrese su contraseña';
+$lang['INGRESE_NOTARJETA']='Ingrese número';
+$lang['ESTATUS'] ='Estatus';
+$lang['NOMBRE']='Nombre';
+$lang['SALDO'] = 'Saldo';
+$lang['MONTO'] = 'Monto';
+$lang['OPCIONES'] = 'Opciones';
+$lang['SEL_ALL'] = 'Seleccionar todo';
