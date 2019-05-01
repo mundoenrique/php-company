7dd7958a23acb0bc56faf71162593d112c99e5d0
Enrique Peñaloza <epenaloza@novopayment.com>
2019-05-01 12:02:58 -0400

    Corrige error en siglas del país  en la uri

--- build/application/config/ec-bp-config.php
+++ build/application/config/ec-bp-config.php
@@ -2,7 +2,7 @@
 
 //País
 $config['country']='Ec-bp';
-$config['countryUri']='ec';
+$config['countryUri']='bp';
 $config['urlWS'] = 'http://172.24.6.78:10003/NovoEolWebInterfaceWS/webresources/';
 /*
 |--------------------------------------------------------------------------
