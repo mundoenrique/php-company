e8fad41ad93fb318d6e1769a735e37861b704751
Jorge Ramirez <jramirez@novopayment.com>
2019-05-13 17:19:39 -0400

    Corrige inserción de submenú vacío

--- build/application/views/layouts/layout-header.php
+++ build/application/views/layouts/layout-header.php
@@ -78,36 +78,34 @@ $style_css = $this->uri->segment(3);
 							<span aria-hidden="true" class="icon" data-icon="<?php echo $lvlOneOpt['icon']?>"></span>
 							<?=$lvlOneOpt['text']?>
 						</a>
-						<ul>
-							<div id="scrollup" style="display:none">
-								<span class="ui-icon ui-icon-triangle-1-n"></span>
-							</div>
-							<?php foreach ($lvlOneOpt['suboptions'] as $lvlTwoOpt): ?>
-								<li>
-									<a href="<?=$lvlTwoOpt['route']?>">
-										<?=$lvlTwoOpt['text']?>
-									</a>
-									<ul>
-										<?php
-										if (isset($lvlTwoOpt['suboptions'])):
-											foreach ($lvlTwoOpt['suboptions'] as $lvlThreeOpt):
-										?>
-												<li>
-													<a href="<?=$lvlThreeOpt['route']?>">
-														<?=$lvlThreeOpt['text']?>
-													</a>
-												</li>
-										<?php
-											endforeach;
-										endif;
-										?>
-									</ul>
-								</li>
-							<?php endforeach; ?>
-							<div id="scrolldown" style="display:none">
-								<span class="ui-icon ui-icon-triangle-1-s"></span>
-							</div>
-						</ul>
+						<?php if (isset($lvlOneOpt['suboptions'])&&!empty($lvlOneOpt['suboptions'])): ?>
+							<ul>
+								<div id="scrollup" style="display:none">
+									<span class="ui-icon ui-icon-triangle-1-n"></span>
+								</div>
+								<?php foreach ($lvlOneOpt['suboptions'] as $lvlTwoOpt): ?>
+									<li>
+										<a href="<?=$lvlTwoOpt['route']?>">
+											<?=$lvlTwoOpt['text']?>
+										</a>
+										<?php if (isset($lvlTwoOpt['suboptions'])&&!empty($lvlTwoOpt['suboptions'])): ?>
+											<ul>
+												<?php foreach ($lvlTwoOpt['suboptions'] as $lvlThreeOpt): ?>
+														<li>
+															<a href="<?=$lvlThreeOpt['route']?>">
+																<?=$lvlThreeOpt['text']?>
+															</a>
+														</li>
+												<?php endforeach; ?>
+											</ul>
+										<? endif; ?>
+									</li>
+								<?php endforeach; ?>
+								<div id="scrolldown" style="display:none">
+									<span class="ui-icon ui-icon-triangle-1-s"></span>
+								</div>
+							</ul>
+						<? endif; ?>
 					</li>
 				<?php endforeach; ?>
 				<li>
