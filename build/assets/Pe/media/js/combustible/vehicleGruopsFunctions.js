b560e49044137af22743cbdce1559ac9baf61efd
Jorge Ramirez <jramirez@novopayment.com>
2019-05-15 15:22:24 -0400

    Añade protección CSRF en peticiones de vehicleGruopsFunctions.js

--- build/assets/Pe/media/js/combustible/vehicleGruopsFunctions.js
+++ build/assets/Pe/media/js/combustible/vehicleGruopsFunctions.js
@@ -6,7 +6,10 @@ function addEdit(id) {
         $('#msg-info').empty();
         $('#msg-info').append('<div id="loading" class="agrups"><img src=" ' + baseCDN + '/media/img/loading.gif' + '"></div>');
         notiSystem('msg', title);
-        $.post(baseURL + '/' + isoPais + '/trayectos/modelo', {way: 'vehicleGroups', modelo: 'vehicleGroups', data: id})
+				var ceo_cook = decodeURIComponent(
+					document.cookie.replace(/(?:(?:^|.*;\s*)ceo_cook\s*\=\s*([^;]*).*$)|^.*$/, '$1')
+				);
+        $.post(baseURL + '/' + isoPais + '/trayectos/modelo', {way: 'vehicleGroups', modelo: 'vehicleGroups', data: id, ceo_name: ceo_cook})
             .done( function(response) {
                 $('#msg-info').empty();
                 switch (response.code) {
