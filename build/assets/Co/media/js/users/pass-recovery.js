fce1eefa16485d6c5bf5f25e6b7b95ed0a766288
Jarvis Rangel <jarangel@novopayment.com>
2019-05-16 09:49:00 -0400

    Incorpora protección CSRF en pass-recovery.js

--- build/assets/Co/media/js/users/pass-recovery.js
+++ build/assets/Co/media/js/users/pass-recovery.js
@@ -44,7 +44,13 @@ $(function(){
             close: function(){$(this).dialog('destroy')},
             resizable:false });
 
-        $.post(baseURL + isoPais+'/users/recuperar-pass', jsonData )
+						var ceo_cook = decodeURIComponent(
+							document.cookie.replace(/(?:(?:^|.*;\s*)ceo_cook\s*\=\s*([^;]*).*$)|^.*$/, '$1')
+							);
+
+						jsonData.ceo_name = ceo_cook
+
+						$.post(baseURL + isoPais+'/users/recuperar-pass', jsonData )
             .done(function( data ) {
             if (data == "validate") {
                 $aux.dialog('destroy');
