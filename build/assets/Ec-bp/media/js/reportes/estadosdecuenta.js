89f60586066e0c4d49dd436c752eb198a3a9437b
Genesis Rodriguez <gerodriguez@novopayment.com>
2019-05-16 07:48:34 -0400

    Incorpora protección CSRF en estadosdecuenta.js

--- build/assets/Ec-bp/media/js/reportes/estadosdecuenta.js
+++ build/assets/Ec-bp/media/js/reportes/estadosdecuenta.js
@@ -52,7 +52,10 @@ $(document).ready(function() {
 			$("#repEstadosDeCuenta_producto").children( 'option:not(:first)' ).remove();
 			$("#cargando_producto").fadeIn("slow");
 			$(this).attr('disabled',true);
-			$.post(baseURL + api + isoPais + "/producto/lista", { 'acrif': acrif }, function(data){
+			var ceo_cook = decodeURIComponent(
+				document.cookie.replace(/(?:(?:^|.*;\s*)ceo_cook\s*\=\s*([^;]*).*$)|^.*$/, '$1')
+			);
+			$.post(baseURL + api + isoPais + "/producto/lista", { 'acrif': acrif, ceo_name: ceo_cook }, function(data){
 				$("#cargando_producto").fadeOut("slow");
 				$("#repEstadosDeCuenta_empresa").removeAttr('disabled');
 				if(!data.ERROR){
@@ -220,8 +223,11 @@ function BuscarEstadosdeCuenta(paginaActual){
 		$("#repEstadosDeCuenta_btnBuscar").hide();
 			$('#div_tablaDetalle').fadeOut("fast");
 
+		var ceo_cook = decodeURIComponent(
+			document.cookie.replace(/(?:(?:^|.*;\s*)ceo_cook\s*\=\s*([^;]*).*$)|^.*$/, '$1')
+		);
 		filtro_busq.paginaActual=paginaActual;
-
+		filtro_busq.ceo_name = ceo_cook;
 		$consulta = $.post(baseURL + api + isoPais + "/reportes/estadosdecuenta",filtro_busq );
 //DE SER EXITOSA LA COMUNICACION CON EL SERVICIO SE EJECUTA EL SIGUIENTE METODO "DONE"
 
@@ -376,6 +382,10 @@ if(buscarReporte){
 		span.click(function(){
 
 //SE EJECUTA LA CONSULTA PARA EL GRAFICO
+				var ceo_cook = decodeURIComponent(
+					document.cookie.replace(/(?:(?:^|.*;\s*)ceo_cook\s*\=\s*([^;]*).*$)|^.*$/, '$1')
+				);
+				filtro_busq.ceo_name = ceo_cook;
 				$consulta = $.post(baseURL + api + isoPais + "/reportes/EstadosdeCuentaGrafico",filtro_busq );
 // APARECE LA VENTANA DE CARGANDO MIENTRAS SE REALIZA LA CONSULTA
 				$( "#cargando" ).dialog({title:"Ver Gráfica Estado de cuenta",modal:true, width: 200, height: 170});
@@ -854,7 +864,10 @@ if(buscarReporte){
 function descargarArchivo(datos, url, titulo){
 
 	$aux = $("#cargando").dialog({title:titulo,modal:true, close:function(){$(this).dialog('close')}, resizable:false });
-
+	var ceo_cook = decodeURIComponent(
+		document.cookie.replace(/(?:(?:^|.*;\s*)ceo_cook\s*\=\s*([^;]*).*$)|^.*$/, '$1')
+	);
+	datos.ceo_name = ceo_cook;
 			$.post(url,datos).done(function(data){
     			$aux.dialog('destroy')
     			if(!data.ERROR){
