367772b3f197eb90e6e7fb063915ffd3d2bda63e
Enrique Peñaloza <epenaloza@novopayment.com>
2019-05-22 01:56:08 -0400

    Excluye URIS de Validación CSRF

--- build/application/config/config.php
+++ build/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = BASE_URL;
+$config['base_url'] = BASE_URL.'/';
 $config['asset_url'] = BASE_CDN_URL;
 $config['asset_path'] = BASE_CDN_PATH;
 $config['favicon'] = 'favicon-novo';
@@ -472,7 +472,14 @@ $config['csrf_token_name'] = 'ceo_name';
 $config['csrf_cookie_name'] = 'cook';
 $config['csrf_expire'] = 7200;
 $config['csrf_regenerate'] = TRUE;
-$config['csrf_exclude_uris'] = array();
+$config['csrf_exclude_uris'] = array(
+	'[\w\-]+/dashboard/productos',
+	'[\w\-]+/dashboard/productos/detalle',
+	'[\w\-]+/consulta/ordenes-de-servicio',
+	'[\w\-]+/lotes/autorizacion/detalle',
+	'[\w\-]+/lotes/confirmacion',
+	'[\w\-]+/lotes/calculo',
+);
 
 /*
 |--------------------------------------------------------------------------
