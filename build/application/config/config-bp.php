2109b81638ad4a54d7b62a46afd4b2aef2ebc87d
Enrique Peñaloza <epenaloza@novopayment.com>
2019-05-23 19:09:38 -0400

    Elimina variable config innecesario

--- build/application/config/config-bp.php
+++ build/application/config/config-bp.php
@@ -1,7 +1,6 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
 
 $config['country'] = 'Ec-bp';
-$config['urlWS'] = $urlWs;
 $config['base_url']	= BASE_URL.'/bp/';
 $config['language']	= 'bp-spanish';
 $config['favicon'] = 'favicon-bp';
