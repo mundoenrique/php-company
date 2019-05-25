62b48c9ca2346ef4135269b6267fe454e0d89dbb
Jorge Ramirez <jramirez@novopayment.com>
2019-05-25 12:22:39 -0400

    Reincorpora validaciones afectadas por incorporación entre ramas

--- build/assets/Ec-bp/media/js/reportes/recargasrealizadas.js
+++ build/assets/Ec-bp/media/js/reportes/recargasrealizadas.js
@@ -101,211 +101,217 @@ $(document).ready(function () {
 		//filtro_busq.paginaActual=1;
 		//filtro_busq.ceo_name = ceo_cook;
 		if (validar_filtro_busqueda("lotes-2")) {
-			$('#cargando').fadeIn("slow");
-			$(this).hide();
-			$('#div_tablaDetalle').fadeOut("fast");
-			filtro_busq.paginaActual = 1;
-			var dataRequest = JSON.stringify({
-				filtro_busq: filtro_busq
+			var form = $('#form-criterio-busqueda');
+			validateForms(form);
+			if (form.valid()) {
+				$('#cargando').fadeIn("slow");
+				$(this).hide();
+				$('#div_tablaDetalle').fadeOut("fast");
+				filtro_busq.paginaActual = 1;
+				var dataRequest = JSON.stringify({
+					filtro_busq: filtro_busq
 
-			})
+				})
 
-			//SE REALIZA LA INVOCACION AJAX
-			dataRequest = CryptoJS.AES.encrypt(dataRequest, ceo_cook, {
-				format: CryptoJSAesJson
-			}).toString();
-			$consulta = $.post(baseURL + api + isoPais + "/reportes/recargasrealizadas", {request: dataRequest, ceo_name: ceo_cook, plot: btoa(ceo_cook) } );
-			//DE SER EXITOSA LA COMUNICACION CON EL SERVICIO SE EJECUTA EL SIGUIENTE METODO "DONE"
-			$consulta.done(function (response) {
-				data = JSON.parse(CryptoJS.AES.decrypt(response.code, response.plot, {format: CryptoJSAesJson}).toString(CryptoJS.enc.Utf8))
-				$("#mensaje").remove();
-				$('#cargando').fadeOut("slow");
-				$("#repRecargasRealizadas_btnBuscar").show();
-				$("#div_tablaDetalle").fadeIn("slow");
-				$("#tabla-datos-general").fadeIn("fast");
-				var tbody = $("#tbody-datos-general");
-				var thead = $('#datos-principales');
-				thead.empty();
-				tbody.empty();
-				var tr;
-				var td;
-				var th;
-				var mes = $("#repRecargasRealizadas_anio").val();
-				mes = mes.split("/")[0];
-				var month = new Array();
-				month[-2] = "Noviembre";
-				month[-1] = "Diciembre";
-				month[0] = "Enero";
-				month[1] = "Febrero";
-				month[2] = "Marzo";
-				month[3] = "Abril";
-				month[4] = "Mayo";
-				month[5] = "Junio";
-				month[6] = "Julio";
-				month[7] = "Agosto";
-				month[8] = "Septiembre";
-				month[9] = "Octubre";
-				month[10] = "Noviembre";
-				month[11] = "Diciembre";
+				//SE REALIZA LA INVOCACION AJAX
+				dataRequest = CryptoJS.AES.encrypt(dataRequest, ceo_cook, {
+					format: CryptoJSAesJson
+				}).toString();
+				$consulta = $.post(baseURL + api + isoPais + "/reportes/recargasrealizadas", {request: dataRequest, ceo_name: ceo_cook, plot: btoa(ceo_cook) } );
+				//DE SER EXITOSA LA COMUNICACION CON EL SERVICIO SE EJECUTA EL SIGUIENTE METODO "DONE"
+				$consulta.done(function (response) {
+					data = JSON.parse(CryptoJS.AES.decrypt(response.code, response.plot, {format: CryptoJSAesJson}).toString(CryptoJS.enc.Utf8))
+					$("#mensaje").remove();
+					$('#cargando').fadeOut("slow");
+					$("#repRecargasRealizadas_btnBuscar").show();
+					$("#div_tablaDetalle").fadeIn("slow");
+					$("#tabla-datos-general").fadeIn("fast");
+					var tbody = $("#tbody-datos-general");
+					var thead = $('#datos-principales');
+					thead.empty();
+					tbody.empty();
+					var tr;
+					var td;
+					var th;
+					var mes = $("#repRecargasRealizadas_anio").val();
+					mes = mes.split("/")[0];
+					var month = new Array();
+					month[-2] = "Noviembre";
+					month[-1] = "Diciembre";
+					month[0] = "Enero";
+					month[1] = "Febrero";
+					month[2] = "Marzo";
+					month[3] = "Abril";
+					month[4] = "Mayo";
+					month[5] = "Junio";
+					month[6] = "Julio";
+					month[7] = "Agosto";
+					month[8] = "Septiembre";
+					month[9] = "Octubre";
+					month[10] = "Noviembre";
+					month[11] = "Diciembre";
 
-				//DE TRAER RESULTADOS LA CONSULTA SE GENERA LA TABLA CON LA DATA...
-				//DE LO CONTRARIO SE GENERA UN MENSAJE "No existe Data relacionada con su filtro de busqueda"
-				th = $(document.createElement("th")).appendTo(thead);
-				th.html("Producto");
-				th = $(document.createElement("th")).appendTo(thead);
-				th.html(month[mes - 3]);
-				th = $(document.createElement("th")).appendTo(thead);
-				th.html(month[mes - 2]);
-				th = $(document.createElement("th")).appendTo(thead);
-				th.html(month[mes - 1]);
-				th = $(document.createElement("th")).appendTo(thead);
-				th.html("Total");
+					//DE TRAER RESULTADOS LA CONSULTA SE GENERA LA TABLA CON LA DATA...
+					//DE LO CONTRARIO SE GENERA UN MENSAJE "No existe Data relacionada con su filtro de busqueda"
+					th = $(document.createElement("th")).appendTo(thead);
+					th.html("Producto");
+					th = $(document.createElement("th")).appendTo(thead);
+					th.html(month[mes - 3]);
+					th = $(document.createElement("th")).appendTo(thead);
+					th.html(month[mes - 2]);
+					th = $(document.createElement("th")).appendTo(thead);
+					th.html(month[mes - 1]);
+					th = $(document.createElement("th")).appendTo(thead);
+					th.html("Total");
 
-				if (data.rc == "0") {
-					$("#view-results").attr("style", "");
-					$.each(data.recargas, function (posLista, itemLista) {
+					if (data.rc == "0") {
+						$("#view-results").attr("style", "");
+						$.each(data.recargas, function (posLista, itemLista) {
+							tr = $(document.createElement("tr")).appendTo(tbody);
+							td = $(document.createElement("td")).appendTo(tr);
+							td.html(itemLista.producto);
+							td = $(document.createElement("td")).appendTo(tr);
+							if (itemLista.montoRecarga1 != null) {
+								td.html(itemLista.montoRecarga1)
+							} else {
+								td.html("0,00")
+							}
+							td.attr("style", "text-align: center");
+							td = $(document.createElement("td")).appendTo(tr);
+							if (itemLista.montoRecarga2 != null) {
+								td.html(itemLista.montoRecarga2)
+							} else {
+								td.html("0,00")
+							}
+							td.attr("style", "text-align: center");
+							td = $(document.createElement("td")).appendTo(tr);
+							if (itemLista.montoRecarga3 != null) {
+								td.html(itemLista.montoRecarga3)
+							} else {
+								td.html("0,00")
+							}
+							td.attr("style", "text-align: center")
+							td = $(document.createElement("td")).appendTo(tr);
+							if (itemLista.totalProducto != null) {
+								td.html(itemLista.totalProducto)
+							} else {
+								td.html("0,00")
+							}
+							td.attr("style", "text-align: center");
+						});
+
 						tr = $(document.createElement("tr")).appendTo(tbody);
 						td = $(document.createElement("td")).appendTo(tr);
-						td.html(itemLista.producto);
+						td.html("Totales");
+						td.attr("style", "text-align: right");
+
 						td = $(document.createElement("td")).appendTo(tr);
-						if (itemLista.montoRecarga1 != null) {
-							td.html(itemLista.montoRecarga1)
-						} else {
-							td.html("0,00")
-						}
+						td.html(data.totalRecargas1);
 						td.attr("style", "text-align: center");
+
 						td = $(document.createElement("td")).appendTo(tr);
-						if (itemLista.montoRecarga2 != null) {
-							td.html(itemLista.montoRecarga2)
-						} else {
-							td.html("0,00")
-						}
+						td.html(data.totalRecargas2);
 						td.attr("style", "text-align: center");
+
 						td = $(document.createElement("td")).appendTo(tr);
-						if (itemLista.montoRecarga3 != null) {
-							td.html(itemLista.montoRecarga3)
-						} else {
-							td.html("0,00")
-						}
-						td.attr("style", "text-align: center")
-						td = $(document.createElement("td")).appendTo(tr);
-						if (itemLista.totalProducto != null) {
-							td.html(itemLista.totalProducto)
-						} else {
-							td.html("0,00")
-						}
+						td.html(data.totalRecargas3);
 						td.attr("style", "text-align: center");
-					});
 
-					tr = $(document.createElement("tr")).appendTo(tbody);
-					td = $(document.createElement("td")).appendTo(tr);
-					td.html("Totales");
-					td.attr("style", "text-align: right");
-
-					td = $(document.createElement("td")).appendTo(tr);
-					td.html(data.totalRecargas1);
-					td.attr("style", "text-align: center");
-
-					td = $(document.createElement("td")).appendTo(tr);
-					td.html(data.totalRecargas2);
-					td.attr("style", "text-align: center");
-
-					td = $(document.createElement("td")).appendTo(tr);
-					td.html(data.totalRecargas3);
-					td.attr("style", "text-align: center");
-
-					td = $(document.createElement("td")).appendTo(tr);
-					td.html(data.totalRecargas);
-					td.attr("style", "text-align: center");
+						td = $(document.createElement("td")).appendTo(tr);
+						td.html(data.totalRecargas);
+						td.attr("style", "text-align: center");
 
 
 
-					$("#grafica").click(function () {
+						$("#grafica").click(function () {
 
-						// SE OBTIENE LAS SERIES
-						var arrayseries = []
-						$.each(data.listaGrafico[0].series, function (posSeries, itemSeries) {
-							var serie = {};
-							let nuevo = [itemSeries.nombreSerie, itemSeries.valores[0]];
-							arrayseries.push(nuevo);
-						});
-						// GRAFICA
-						$('#chart').highcharts({
-							chart: {
-								type: 'column',
-								plotBackgroundColor: null,
-								plotBorderWidth: null,
-								plotShadow: false
-							},
-							plotOptions: {
-								series: {
-									colorByPoint: true
-								}
-							},
-							title: {
-								text: data.recargas[0].producto
-							},
-							xAxis: {
-								type: 'category'
-							},
-							yAxis: {
-								min: 0,
-								title: {
-									text: 'Recargas realizadas'
-								}
-							},
-							legend: {
-								enabled: false
-							},
-							tooltip: {
-								pointFormat: 'Recargas: <b>{point.y}</b>'
-							},
-							series: [{
-								name: 'Population',
-								data: arrayseries,
-								dataLabels: {
-									enabled: true,
-									rotation: 0,
-									color: '#000000',
-									align: 'center',
-									format: '{point.y}',
-									y: 2,
-									style: {
-										fontSize: '13px'
+							// SE OBTIENE LAS SERIES
+							var arrayseries = []
+							$.each(data.listaGrafico[0].series, function (posSeries, itemSeries) {
+								var serie = {};
+								let nuevo = [itemSeries.nombreSerie, itemSeries.valores[0]];
+								arrayseries.push(nuevo);
+							});
+							// GRAFICA
+							$('#chart').highcharts({
+								chart: {
+									type: 'column',
+									plotBackgroundColor: null,
+									plotBorderWidth: null,
+									plotShadow: false
+								},
+								plotOptions: {
+									series: {
+										colorByPoint: true
 									}
-								}
-							}]
-						});
+								},
+								title: {
+									text: data.recargas[0].producto
+								},
+								xAxis: {
+									type: 'category'
+								},
+								yAxis: {
+									min: 0,
+									title: {
+										text: 'Recargas realizadas'
+									}
+								},
+								legend: {
+									enabled: false
+								},
+								tooltip: {
+									pointFormat: 'Recargas: <b>{point.y}</b>'
+								},
+								series: [{
+									name: 'Population',
+									data: arrayseries,
+									dataLabels: {
+										enabled: true,
+										rotation: 0,
+										color: '#000000',
+										align: 'center',
+										format: '{point.y}',
+										y: 2,
+										style: {
+											fontSize: '13px'
+										}
+									}
+								}]
+							});
 
-						$("#chart").dialog({
-							modal: true,
-							width: 700,
-							height: 400
-						});
-						$("#chart").height(400);
-						$("#chart svg").height(385);
+							$("#chart").dialog({
+								modal: true,
+								width: 700,
+								height: 400
+							});
+							$("#chart").height(400);
+							$("#chart svg").height(385);
 
-					});
-					$('#tabla-datos-general tbody tr:even').addClass('even ');
-				} else {
-					if (data.rc == "-29") {
-						alert("Usuario actualmente desconectado");
-						$(location).attr('href', baseURL + isoPais + '/login');
+						});
+						$('#tabla-datos-general tbody tr:even').addClass('even ');
 					} else {
-						$("#mensaje").remove();
-						var contenedor = $("#div_tablaDetalle");
-						$("#tabla-datos-general").fadeOut("fast");
-						$("#view-results").attr("style", "display:none");
-						var div = $(document.createElement("div")).appendTo(contenedor);
-						div.attr("id", "mensaje");
-						div.attr("style", "background-color:rgb(252,199,199); margin-top:43px;");
-						var p = $(document.createElement("p")).appendTo(div);
-						p.html(data.mensaje);
-						p.attr("style", "text-align:center;padding:10px;font-size:14px");
+						if (data.rc == "-29") {
+							alert("Usuario actualmente desconectado");
+							$(location).attr('href', baseURL + isoPais + '/login');
+						} else {
+							$("#mensaje").remove();
+							var contenedor = $("#div_tablaDetalle");
+							$("#tabla-datos-general").fadeOut("fast");
+							$("#view-results").attr("style", "display:none");
+							var div = $(document.createElement("div")).appendTo(contenedor);
+							div.attr("id", "mensaje");
+							div.attr("style", "background-color:rgb(252,199,199); margin-top:43px;");
+							var p = $(document.createElement("p")).appendTo(div);
+							p.html(data.mensaje);
+							p.attr("style", "text-align:center;padding:10px;font-size:14px");
+						}
 					}
-				}
 
-			});
+				});
+			} else {
+				showErrMsg('Verifique los datos ingresados e intente nuevamente');
+			}
 		}
 
 	});
