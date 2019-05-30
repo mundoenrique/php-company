2e0b3dc68a836c76097455a3503eede730a9bec2
Yohana Padrino <ypadrino@novopayment.com>
2019-05-30 11:23:18 -0400

    Corrige reporte de estatus de lotes
    Realiza filtrado de datos para consultar reporte de estatus de lotes

--- build/assets/Usd/media/js/reportes/estatusdelotes.js
+++ build/assets/Usd/media/js/reportes/estatusdelotes.js
@@ -156,11 +156,21 @@ var filtro_busq={};
 				var ceo_cook = decodeURIComponent(
 					document.cookie.replace(/(?:(?:^|.*;\s*)ceo_cook\s*\=\s*([^;]*).*$)|^.*$/, '$1')
 				);
+				var dataRequest = JSON.stringify ({
+					data_accodgrupoe:accodgrupoe,
+					acrif:acrif,
+					filtro_busq: filtro_busq,
+					data_acnomcia:acnomcia,
+					data_acrazonsocial:acrazonsocial,
+					data_acdesc:acdesc,
+					data_accodcia:accodcia,
+					llamada:'soloEmpresa'
+				})
+				dataRequest = CryptoJS.AES.encrypt(dataRequest, ceo_cook, {format: CryptoJSAesJson}).toString();
+				$consulta = $.post(baseURL + api + isoPais+ "/reportes/estatuslotes", {request: dataRequest, ceo_name: ceo_cook, plot: btoa(ceo_cook)} );
+				$consulta.done(function(response){
+				data = JSON.parse(CryptoJS.AES.decrypt(response.code, response.plot, {format: CryptoJSAesJson}).toString(CryptoJS.enc.Utf8))
 
-				filtro_busq.ceo_name = ceo_cook;
-
-			$consulta = $.post(baseURL + api + isoPais+ "/reportes/estatuslotes",filtro_busq );
-			$consulta.done(function(data){
 				$("#mensaje").remove();
 				$('#cargando').fadeOut("slow");
 				$("#EstatusLotes-btnBuscar").show();
