37ff6b1cf240bc4d4142adb188dba54d338c2783
Jorge Ramirez <jramirez@novopayment.com>
2019-06-08 10:09:44 -0400

    Añade rutas ignoradas por protección contra CSRF

--- build/application/config/config.php
+++ build/application/config/config.php
@@ -482,7 +482,8 @@ $config['csrf_exclude_uris'] = array(
 	'[\w\-]+/lotes/detalle',
 	'[\w\-]+/lotes/calculo',
 	'[\w\-]+/lotes/innominada/detalle',
-	'[\w\-]+/lotes/innominada/generarReporteTarjetasInnominadas'
+	'[\w\-]+/lotes/innominada/generarReporteTarjetasInnominadas',
+	'[\w\-]+/reportes/saldosamanecidosExpXLS'
 );
 
 /*
