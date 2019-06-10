5c2c0b4e7d4065e5414c679aee229cef30378106
Genesis Rodriguez <gerodriguez@novopayment.com>
2019-06-10 09:34:23 -0400

    Incorpora cifrado y descifrado en Co: lotes-autorización.js

--- build/assets/Co/media/js/lotes/lotes-autorizacion.js
+++ build/assets/Co/media/js/lotes/lotes-autorizacion.js
@@ -252,8 +252,10 @@ $item = $(this);
 					 var ceo_cook = decodeURIComponent(
 						document.cookie.replace(/(?:(?:^|.*;\s*)ceo_cook\s*\=\s*([^;]*).*$)|^.*$/, '$1')
 						);
-
-					 $.post(baseURL+isoPais+'/lotes/autorizacion/desasociar',{'data-lotes': idlote,'data-pass':pass, ceo_name: ceo_cook}).done( function(data){
+						var dataRequest = JSON.stringify({'data_lotes':idlote, 'data_pass':pass});
+						dataRequest = CryptoJS.AES.encrypt(dataRequest, ceo_cook, {format: CryptoJSAesJson}).toString();
+            $.post(baseURL+isoPais+'/lotes/autorizacion/desasociar',{request: dataRequest, ceo_name: ceo_cook, plot: btoa(ceo_cook)}).done( function(response){
+							var data = JSON.parse(CryptoJS.AES.decrypt(response.code, response.plot, {format: CryptoJSAesJson}).toString(CryptoJS.enc.Utf8));
               $aux.dialog('destroy');
                if(!data.ERROR){
 
