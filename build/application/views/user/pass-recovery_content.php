82ffb5fd7da9b25aab029bd098cf9041db8eef4c
Yohana Padrino <ypadrino@novopayment.com>
2019-07-11 15:48:34 -0400

    posiciona boton continuar en recuperar contraseña y centra estos botones para pichincha

--- build/application/views/user/pass-recovery_content.php
+++ build/application/views/user/pass-recovery_content.php
@@ -1,68 +1,11 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+$pais= $this->uri->segment(1, 0);
+?>
 <section class="container">
 	<header class="padding-left-right">
 		<h1>Recuperar contraseña</h1>
 	</header>
-	<style>
-
-
-/**Estilos de los botones NOVO NEW****/
-
-.novo-btn-primary,.novo-btn-secondary{
-	width:120px;
-	padding-left:15px;
-	padding-right:15px;
-}
-.novo-btn-primary-modal,.novo-btn-secondary-modal{
-	width:68px;
-	padding-left:15px;
-	padding-right:15px;
-}
-
-.novo-btn-primary:hover{
-	background: url('../img/BackgroundButtonHover.png') left center no-repeat #ffdd00;
-}
-.novo-btn-primary:active{
-	background: #ffd000;
-}
-.novo-btn-primary-modal:hover{
-	background: url('../img/BackgroundButtonHover.png') left center no-repeat #ffdd00;
-}
-
-.novo-btn-secondary,.novo-btn-secondary-modal{
-	border: 1px solid #ffdd00;
-	background:white !important;
-}
-/* .novo-btn-secondary-modal{
-	background:white;
-} */
-.novo-btn-secondary:hover{
-	border: 1px solid #ffdd00 !important;
-	background-color: #fffede !important;
-}
-.novo-btn-secondary:active{
-	border: 1px solid #ffdd00 !important;
-	color:#10133f;
-	background-color: #fdfbb5 !important;
-}
-
-.novo-btn-secondary-modal:hover{
-
-	background:#fffede !important;
-	border: 1px solid #ffdd00 !important;
-}
-.novo-btn-secondary-modal:active{
-
-	background:#fdfbb5 !important;
-	border: 1px solid #ffdd00 !important;
-}
-.btn-authorization{
-	margin-bottom: 10px;
-	margin-top: 10px;
-	border: 1px solid #ffdd00;
-}
-
-	</style>
 	<article class="padding-left-right">
 		<p class="paragraph"><?= lang('FORGOT_PASS'); ?></p>
 		<form id="form-pass-recovery" name="form-pass-recovery" accept-charset="utf-8">
@@ -84,23 +27,40 @@
 			</fieldset>
 			<div class="form-actions">
 
-			<?php
-			//	echo "sdjkha".base_url('inicio');
-			$pais=$this->urlCountry = $this->uri->segment(1, 0);
-				if($pais=='bp'){
-					?>
-						<center>
-					<?php
-				}
-			?>
-			<button class="novo-btn-secondary">
-				<a class="cancel-anchor  r-button " href="<?= base_url('inicio') ?>" style="display:inline;height:;">Cancelar</a></button>
-				<button id="btn-pass-recover" class="r-button novo-btn-primary">Continuar</button>
-				<?php if($pais=='bp'){
-					?>
-						</center>
-					<?php
-				}?>
+			<?php	if($pais!='bp'): ?>
+				<table style="float:right;">
+			<?php else: ?>
+				<center>
+				<table>
+			<?php endif; ?>
+					<tr>
+						<td valign="top">
+						<?php	if($pais=='bp'): ?>
+								<center>
+						<?	endif; ?>
+								<a class="cancel-anchor novo-btn-secondary novo-cancel-pass-recovery" href="<?= base_url('inicio') ?>">Cancelar</a>
+						<?php	if($pais=='bp'): ?>
+						<?php else:
+								?>
+								<button id="btn-pass-recover" class="novo-btn-primary">Continuar</button><?php
+						endif; ?>
+						</td>
+						<td valign="top">
+						<?php	if($pais=='bp'): ?>
+								<button id="btn-pass-recover" class="novo-btn-primary">
+								<a class="cancel-anchor" href="<?= base_url('inicio') ?>">Continuar</a>
+								</button>
+								</center>
+						<?	endif; ?>
+					 </td>
+					</tr>
+				</table>
+			<?php	if($pais=='bp'): ?>
+			<center>
+			<?php endif; ?>
+<div class="content-t">
+
+</div>
 			</div>
 		</form>
 	</article>
