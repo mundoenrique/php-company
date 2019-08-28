db3da607f375b666c446c8b535ccdf3c11673258
jtavio <jtavio@novopayment.com>
2019-08-28 11:43:56 -0400

    modifica color del boton aceptar en lotes

--- build/assets/Ec-bp/media/js/lotes/lotes.js
+++ build/assets/Ec-bp/media/js/lotes/lotes.js
@@ -419,7 +419,7 @@ $(function () { // Document ready
 			buttons: {
 				"Aceptar": {
 					text: 'Aceptar',
-					class: 'novo-btn-primary-modal aqui',
+					class: 'novo-btn-primary-modal',
 					click: function () {
 					$(this).dialog("close");
 					}
