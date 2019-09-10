1172bcae15c3334c4e20ae141c9721a577ec6ad2
Pedro Torres <ptorres@novopayment.com>
2019-09-10 12:32:43 -0400

    corrige el comportamiento en los imputs de login

--- build/assets/js/user/login.js
+++ build/assets/js/user/login.js
@@ -1,6 +1,45 @@
 'use strict'
+
 $(function() {
 	var data;
+
+	const responseCodeLogin = {
+		0: function(response){
+			$(location).attr('href', response.data)
+		},
+		1: function(response, textBtn){
+			restartFormLogin(textBtn);
+			$('#user_login').showBalloon({
+				html: true,
+				classname: response.className,
+				position: "left",
+				contents: response.msg
+			});
+		},
+		2: function(){
+			user.active = 1;
+			verb = "POST"; who = 'User'; where = 'Login'; data = getCredentialsUser();
+			callNovoCore(verb, who, where, data, function(response) {
+				validateResponseLogin(response);
+			})
+		},
+		3: function(response, textBtn){
+			var dataLogin = getCredentialsUser();
+			notiSystem(response.title, response.msg, response.icon, response.data);
+			var btn = response.data.btn1;
+			if(btn.action == 'logout') {
+				$('#accept').on('click', function() {
+					verb = 'POST'; who = btn.link.who; where = btn.link.where; data = dataLogin;
+					callNovoCore (verb, who, where, data);
+				});
+			}
+			restartFormLogin(textBtn);
+		},
+		99: function(response){
+			notiSystem(response.title, response.msg, response.icon, response.data);
+			restartFormLogin(textBtn);
+		}
+	}
 
 	function disabledInputsform(disable){
 		$('#login-form input, #login-form button').attr('disabled', disable);
@@ -28,44 +67,6 @@ $(function() {
 						}
 	};
 
-	const responseCodeLogin = {
-		0: function(response){
-			$(location).attr('href', response.data)
-		},
-		1: function(response, textBtn){
-			$('#user_login').showBalloon({
-				html: true,
-				classname: response.className,
-				position: "left",
-				contents: response.msg
-			});
-			restartFormLogin(textBtn);
-		},
-		2: function(){
-			user.active = 1;
-			verb = "POST"; who = 'User'; where = 'Login'; data = getCredentialsUser();
-			callNovoCore(verb, who, where, data, function(response) {
-				validateResponseLogin(response);
-			})
-		},
-		3: function(response, textBtn){
-			var dataLogin = getCredentialsUser();
-			notiSystem(response.title, response.msg, response.icon, response.data);
-			var btn = response.data.btn1;
-			if(btn.action == 'logout') {
-				$('#accept').on('click', function() {
-					verb = 'POST'; who = btn.link.who; where = btn.link.where; data = dataLogin;
-					callNovoCore (verb, who, where, data);
-				});
-			}
-			restartFormLogin(textBtn);
-		},
-		99: function(response){
-			notiSystem(response.title, response.msg, response.icon, response.data);
-			disabledInputsform(false);
-		}
-	}
-
 	function validateLogin(token,user,text){
 		data = {
 			user: user.user,
@@ -76,13 +77,12 @@ $(function() {
 		// verb = "POST"; who = 'User'; where = 'Login'; data = user; // llama al login
 		callNovoCore(verb, who, where, data, function(response) {
 
-			$('#login-btn').html(text);
+			//$('#login-btn').html(text);
 
 			if (response.code !== 0 && response.owner === 'captcha'){
 
 				notiSystem(response.title, response.msg, response.icon, response.data);
-				disabledInputsform(false);
-				$('#login-btn').html(text);
+				restartFormLogin(text);
 
 				setTimeout(function() {
 					$("#user_login").hideBalloon();
@@ -114,10 +114,11 @@ $(function() {
 
 		if(form.valid()) {
 			$(this).html(loader);
+
 			grecaptcha.ready(function() {
 				grecaptcha
 				.execute('6Lejt6MUAAAAANd7KndpsZ2mRSQXuYHncIxFJDYf', {action: 'login'})
-				.then(function(token) {
+				.then(function(token ) {
 					validateLogin(token, user, text);
 				}, function(token) {
 					if(!token) {
@@ -131,12 +132,12 @@ $(function() {
 								action: 'close'
 							}
 						};
+						restartFormLogin(text);
 						notiSystem(title, msg, icon, data);
-						disabledInputsform(false);
-						$('#login-btn').html(text);
 					}
 				});
 			});
+
 		} else {
 
 			if (user.user.trim() || user.pass.trim()){
