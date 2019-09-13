1efe06facb8d4fb7b3157e64ddbb8307c055140f
Pedro Torres <ptorres@novopayment.com>
2019-09-13 16:11:36 -0400

    creacion de lenguages para la vista de login

---
+++ build/application/language/us-spanish/login_lang.php
@@ -0,0 +1,8 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['SYSTEM_NAME']= "Conexión Empresas Online Perú";
+$lang['LOGIN_INFO']='Ventas e información a empresas';
+$lang['INFO-1']='Teléfono: (51-1) 619-8900';
+$lang['INFO-2']='Fax: (51-1) 619-8900 anexo 200';
+$lang['INFO-3']='E-mail: ventas@tebca.com.pe';
