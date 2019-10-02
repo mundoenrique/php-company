9183fc9a1e283316bb744c070d5e023e834f5353
epenaloza <epenaloza@novopayment.com>
2019-10-02 07:15:08 -0400

    Crea variables de lenguaje para nueva estructura

---
+++ build/application/language/spanish-bp/login_lang.php
@@ -0,0 +1,5 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['LOGIN_WELCOME_TITLE'] = 'Bienvenido a Conexión Empresas';
+$lang['LOGIN_WELCOME_MESSAGE']='Con este sistema las gestiones de pago de tu empresa son más eficientes, seguras y rápidas.<br>Empieza a disfrutar de todas las ventajas que te ofrece Conexión Empresas.';
---
+++ build/application/language/spanish-ve/pass-recovery_lang.php
@@ -0,0 +1,6 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['RIF_NIT']= "RIF de la Empresa";
+$lang['PLACE_HOLDER_NIT'] = "J-00000000-0";
+
