63e55475041e4903132ceed037d729f53beb9318
epenaloza <epenaloza@novopayment.com>
2019-10-02 07:17:37 -0400

    Cambia key de variable de lenguaje

--- build/application/views/information/rates_content.php
+++ build/application/views/information/rates_content.php
@@ -1,5 +1,5 @@
 <div id="content-beneficioss">
-	<h1><?= lang('RATE_TITLE'); ?></h1>
+	<h1><?= lang('SUBMENU_TARIFAS'); ?></h1>
 	<div class="content-beneficios">
 		<div class="content-tarifas">
 			<?php
