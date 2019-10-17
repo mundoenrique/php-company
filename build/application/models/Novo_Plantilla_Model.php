e8d8f841d7738106fbcd68e98f343ebbe4553af8
epenaloza <epenaloza@novopayment.com>
2019-10-17 18:11:45 -0400

    Quita switch innecesario

--- build/application/models/Novo_Plantilla_Model.php
+++ build/application/models/Novo_Plantilla_Model.php
@@ -29,36 +29,35 @@ class Novo_Plantilla_Model extends NOVO_Model {
 		$this->dataRequest->userName = mb_strtoupper($dataRequest->user);
 
 		$response = $this->sendToService('Plantilla');
-		if($this->isResponseRc !== FALSE) {
-			switch($this->isResponseRc) {
-				case 0:
-					log_message('DEBUG', 'NOVO ['.$dataRequest->user.'] RESPONSE: Login: ' . json_encode($response->usuario));
 
-					break;
-				case -5000:
-					$this->response->code = 1;
-					$this->response->title = lang('REEMPLAZAR POR TRADUCCION DESDE RESPONSE-LANG');
-					$this->response->className = 'error-login-2';
-					$this->response->msg = lang('REEMPLAZAR POR TRADUCCION DESDE RESPONSE-LANG');
-					break;
-				case -6000:
-					$this->response->code = 3;
-					$this->response->msg = lang('REEMPLAZAR POR TRADUCCION DESDE RESPONSE-LANG');
-					$this->response->icon = 'ui-icon-info';
-					$this->response->data = [
-						'btn1'=> [
-							'text'=> lang('BUTTON-ACCEPT'),
-							'link'=> base_url('inicio'),
-							'action'=> 'redirect'
-						],
-						'btn2'=> [
-							'text'=> lang('BUTTON-CANCEL'),
-							'link'=> FALSE,
-							'action'=> 'close'
-						]
-					];
-					break;
-			}
+		switch($this->isResponseRc) {
+			case 0:
+				log_message('DEBUG', 'NOVO ['.$dataRequest->user.'] RESPONSE: Login: ' . json_encode($response->usuario));
+
+				break;
+			case -5000:
+				$this->response->code = 1;
+				$this->response->title = lang('REEMPLAZAR POR TRADUCCION DESDE RESPONSE-LANG');
+				$this->response->className = 'error-login-2';
+				$this->response->msg = lang('REEMPLAZAR POR TRADUCCION DESDE RESPONSE-LANG');
+				break;
+			case -6000:
+				$this->response->code = 3;
+				$this->response->msg = lang('REEMPLAZAR POR TRADUCCION DESDE RESPONSE-LANG');
+				$this->response->icon = 'ui-icon-info';
+				$this->response->data = [
+					'btn1'=> [
+						'text'=> lang('BUTTON-ACCEPT'),
+						'link'=> base_url('inicio'),
+						'action'=> 'redirect'
+					],
+					'btn2'=> [
+						'text'=> lang('BUTTON-CANCEL'),
+						'link'=> FALSE,
+						'action'=> 'close'
+					]
+				];
+				break;
 		}
 
 		return $this->response;
