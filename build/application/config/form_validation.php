b8fa192b36bcfd0e1aa3846bceb198782430b3d3
epenaloza <epenaloza@novopayment.com>
2019-10-28 19:49:58 -0400

    Cambia nombre de regla de validación

--- build/application/config/form_validation.php
+++ build/application/config/form_validation.php
@@ -59,7 +59,7 @@ $config = [
 			'rules' => 'trim|regex_match[/^([a-zA-Z]+[0-9_.+-]*)+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/]|required'
 		]
 	],
-	'changepassword' => [
+	'changePassword' => [
 		[
 			'field' => 'currentPass',
 			'label' => 'currentPass',
