df177d1a249189a1bda68cd146950b872db5f3f9
epenaloza <epenaloza@novopayment.com>
2019-10-28 09:51:45 -0400

    Elimina archivos innecesarios

--- build/application/language/bp-spanish/dashboard_lang.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-//ENTERPRISE_CONTENT
-$lang['ALL'] = 'Todos';
-$lang['FIRST_PAGE'] = 'Primera';
-$lang['LAST_PAGE'] = 'Última';
--- build/application/language/bp-spanish/general_lang.php
+++
@@ -1,6 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-$lang['SYSTEM_NAME']= 'Conexión Empresas';
-$lang['LOGO-HEAD'] = 'logo-pichincha-azul.png';
-$lang['BREADCRUMB_WELCOME'] = 'Bienvenido/a';
