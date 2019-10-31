5e4c311a1e2eb781440c972ae4456f915ec66290
epenaloza <epenaloza@novopayment.com>
2019-10-31 10:53:27 -0400

    Realiza llamado a libreria de validación

--- build/application/controllers/CallModels.php
+++ build/application/controllers/CallModels.php
@@ -31,51 +31,16 @@ class CallModels extends Novo_Controller {
 			unset($this->dataRequest);
 		}
 
-		$result = $this->form_validation->run($this->rule);
-		log_message('DEBUG', 'NOVO VALIDATION FORM '.$this->rule.': '.json_encode($result));
-		if($result) {
-			foreach ($_POST AS $key => $value) {
-				switch($key) {
-					case 'request':
-					case 'plot':
-						continue;
-					default:
-					$this->request->$key = $value;
-				}
-			}
-			unset($_POST);
-			languageLoad(NULL, $this->rule);
-			$this->config->set_item('language', 'spanish-'.$this->countryUri);
-			languageLoad($this->countryUri, $this->rule);
-			$this->load->model($this->model, 'modelLoad');
-			$method = $this->method;
-			$this->dataResponse = $this->modelLoad->$method($this->request);
+		$valid = $this->verify_access->validateForm($this->rule, $this->countryUri);
 
+		if($valid) {
+			$this->request = $this->verify_access->createRequest();
+			$this->dataResponse = $this->loadModel($this->request);
 		} else {
-			log_message('DEBUG', 'NOVO VALIDATION ERRORS: '.json_encode(validation_errors()));
-			$this->dataResponse->code = lang('RESP_DEFAULT_CODE');;
-			$this->dataResponse->title = lang('GEN_SYSTEM_NAME');
-			$this->dataResponse->msg = lang('RESP_VALIDATION_INPUT');
-			$this->dataResponse->data = base_url('inicio');
-			$this->dataResponse->icon = 'ui-icon-alert';
-			$this->dataResponse->data = [
-				'btn1'=> [
-					'text'=> lang('GEN_BTN_ACCEPT'),
-					'link'=> base_url('inicio'),
-					'action'=> 'redirect'
-				]
-			];
-			$this->session->sess_destroy();
+			$this->dataResponse = $this->verify_access->ResponseByDefect();
 		}
-		$data = $this->dataResponse->data;
-		$dataLink = isset($data['btn1']['link']) ? $data['btn1']['link'] : FALSE;
-		if(!is_array($data) && strpos($data, 'dashboard') !== FALSE) {
-			$data = str_replace($this->countryUri.'/', $this->config->item('country').'/', $data);
-		} elseif($dataLink && !is_array($dataLink) && strpos($dataLink, 'dashboard') !== FALSE) {
-			$dataLink = str_replace($this->countryUri.'/', $this->config->item('country').'/', $dataLink);
-			$data['btn1']['link'] =  $dataLink;
-		}
-		$this->dataResponse->data = $data;
+
+		$this->dataResponse->data = $this->verify_access->validateRedirect($this->dataResponse->data, $this->countryUri);
 		$dataResponse = $this->cryptography->encrypt($this->dataResponse);
 		$this->output->set_content_type('application/json')->set_output(json_encode($dataResponse));
 	}
