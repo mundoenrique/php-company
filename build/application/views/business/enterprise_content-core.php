c55651666a6dbb9c409a29708c86a79f3a011786
epenaloza <epenaloza@novopayment.com>
2019-10-31 20:50:13 -0400

    Maneja archivos

---
+++ build/application/views/business/enterprise_content-core.php
@@ -0,0 +1,112 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<span class="primary">Buenos dias Pedro Pérez</span>
+<div class="flex items-center light mb-2">
+	<div class="flex tertiary">
+		<nav class="main-nav nav-inferior ">
+			<ul class="list-style-none light h6 list-reset tertiary mb-0">
+				<li class="inline"><a class="tertiary" href="ceo_dashboard.html">Inicio /</a></li>
+				<li class="inline"><a class="tertiary" href="ceo_dashboard.html">Empresas</a></li>
+			</ul>
+		</nav>
+	</div>
+	<div class="flex flex-auto justify-end h6 ">
+		<span>Último acceso: 11/08/2019 a las 19:12:35</span>
+	</div>
+</div>
+
+<div class="flex items-center light mt-3">
+	<div class="flex h5">
+		<span>Total Empresas: 4</span>
+	</div>
+	<div class="flex flex-auto justify-end h6">
+		<button class="btn btn-outline btn-small bg-white btn-rounded-left">
+			TODOS
+		</button>
+		<button class="btn-options bg-white bold">A-C</button>
+		<button class="btn-options bg-white bold">D-G</button>
+		<button class="btn-options bg-white bold">H-K</button>
+		<button class="btn-options bg-white bold">L-O</button>
+		<button class="btn-options bg-white bold">T-W</button>
+		<button class="btn-options bg-white bold">X-Z</button>
+		<button class="btn-search bg-white"></button>
+	</div>
+</div>
+<div class="line mt-1"></div>
+
+<div class="flex flex-auto flex-wrap m-5 p-2 justify-between">
+	<a href="ceo_product.html">
+		<div class="order-1 card bg-white">
+			<div class="flex flex-column px-1 py-3 mx-1 producto prod-first">
+				<span class="h5 semibold">EMPRESA DE ENERGIA</span>
+				<span class="h6 light gray my-1">Empresa de Servicios Públicos / Bogotá - DC</span>
+				<span class="h5 regular tertiary pt-1">NIT: J-00000000-9</span>
+				<div class="mask flex flex-column mx-1 mt-5 pt-2 tertiary bg-white">
+					<span class="product h5">2 Productos</span>
+					<span class="product h5">Sucursales</span>
+					<span class="product h5">Lorem Ipsum</span>
+				</div>
+			</div>
+		</div>
+	</a>
+
+	<div class="order-1 card bg-white">
+		<div class="flex flex-column px-1 py-3 mx-1 producto prod-first">
+			<span class="h5 semibold primary">EMPRESA DE ENERGIA</span>
+			<span class="h6 light gray my-1">Empresa de Servicios Públicos / Bogotá - DC</span>
+			<span class="h5 regular tertiary pt-1">NIT: J-00000000-9</span>
+			<div class="mask flex flex-column mx-1 mt-5 pt-2 tertiary bg-white">
+				<span class="product h5">2 Productos</span>
+				<span class="product h5">Sucursales</span>
+				<span class="product h5">Lorem Ipsum</span>
+			</div>
+		</div>
+	</div>
+	<div class="order-1 card bg-white">
+		<div class="flex flex-column px-1 py-3 mx-1 producto prod-first">
+			<span class="h5 semibold primary">EMPRESA DE ENERGIA</span>
+			<span class="h6 light gray my-1">Empresa de Servicios Públicos / Bogotá - DC</span>
+			<span class="h5 regular tertiary pt-1">NIT: J-00000000-9</span>
+			<div class="mask flex flex-column mx-1 mt-5 pt-2 tertiary bg-white">
+				<span class="product h5">2 Productos</span>
+				<span class="product h5">Sucursales</span>
+				<span class="product h5">Lorem Ipsum</span>
+			</div>
+		</div>
+	</div>
+	<div class="order-1 card bg-white">
+		<div class="flex flex-column px-1 py-3 mx-1 producto prod-first">
+			<span class="h5 semibold primary">EMPRESA DE ENERGIA</span>
+			<span class="h6 light gray my-1">Empresa de Servicios Públicos / Bogotá - DC</span>
+			<span class="h5 regular tertiary pt-1">NIT: J-00000000-9</span>
+			<div class="mask flex flex-column mx-1 mt-5 pt-2 tertiary bg-white">
+				<span class="product h5">2 Productos</span>
+				<span class="product h5">Sucursales</span>
+				<span class="product h5">Lorem Ipsum</span>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div>
+	<div class="flex flex-auto justify-center my-5 py-5">
+		<nav class="h4">
+			<a href="#">Primera</a>
+			&nbsp;
+			<a href="#">««</a>
+			&nbsp;
+			<a href="#">«</a>
+		</nav>
+
+		<div class="h4">
+			<span><a href="javascript:">1</a></span>
+		</div>
+
+		<nav class="h4">
+			<a href="#">»</a>
+			&nbsp;
+			<a href="#">»»</a>
+			&nbsp;
+			<a href="#">Última</a>
+		</nav>
+	</div>
+</div>
