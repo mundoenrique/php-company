163839ebe1305e20770d6c8edb8400a57efa0be0
epenaloza <epenaloza@novopayment.com>
2019-11-06 19:20:48 -0400

    Quita ejemplo de isotope JS

--- build/assets/js/business/enterprise.js
+++ build/assets/js/business/enterprise.js
@@ -59,7 +59,7 @@ $(function () {
 	$('.pagination').pagination({
 		dataSource: function(done){
 			var result = [];
-			for (var i = 1; i <= 2; i++) {
+			for (var i = 1; i <= 15; i++) {
 					result.push(i);
 			}
 			console.log(result)
@@ -68,85 +68,4 @@ $(function () {
 	 pageRange: 2,
 	 pageSize: 4
 	})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	// init Isotope
-	var $grid = $('.grid').isotope({
-		itemSelector: '.element-item',
-		layoutMode: 'fitRows',
-		getSortData: {
-			name: '.name',
-			symbol: '.symbol',
-			number: '.number parseInt',
-			category: '[data-category]',
-			weight: function( itemElem ) {
-				var weight = $( itemElem ).find('.weight').text();
-				return parseFloat( weight.replace( /[\(\)]/g, '') );
-			}
-		}
-	});
-
-	// filter functions
-	var filterFns = {
-		// show if number is greater than 50
-		numberGreaterThan50: function() {
-			var number = $(this).find('.number').text();
-			return parseInt( number, 10 ) > 50;
-		},
-		// show if name ends with -ium
-		ium: function() {
-			var name = $(this).find('.name').text();
-			return name.match( /ium$/ );
-		}
-	};
-
-	// bind filter button click
-	$('#filters').on( 'click', 'button', function() {
-		var filterValue = $( this ).attr('data-filter');
-		// use filterFn if matches value
-		filterValue = filterFns[ filterValue ] || filterValue;
-		$grid.isotope({ filter: filterValue });
-	});
-
-	// bind sort button click
-	$('#sorts').on( 'click', 'button', function() {
-		var sortByValue = $(this).attr('data-sort-by');
-		$grid.isotope({ sortBy: sortByValue });
-	});
-
-	// change is-checked class on buttons
-	$('.button-group').each( function( i, buttonGroup ) {
-		var $buttonGroup = $( buttonGroup );
-		$buttonGroup.on( 'click', 'button', function() {
-			$buttonGroup.find('.is-checked').removeClass('is-checked');
-			$( this ).addClass('is-checked');
-		});
-	});
-
-
 });
