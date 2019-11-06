a1a39c59f3a34422e73c882048bf5af7f4a467d6
jtavio <jtavio@novopayment.com>
2019-11-06 11:39:38 -0400

    agrega codigo para visualizar el widget

--- build/assets/Ec-bp/media/js/lotes/lotes.js
+++ build/assets/Ec-bp/media/js/lotes/lotes.js
@@ -4,6 +4,17 @@ $(function () { // Document ready
 	var ceo_cook;
 	$('#lotes-2').show();
 	$(".aviso").removeClass("elem-hidden");
+
+	var top = ($('#sidebar-products').offset().top - 100) - parseFloat($('#sidebar-products').css('marginTop').replace(/auto/, 0));
+       $(window).scroll(function (event) {
+         var y = $(this).scrollTop();
+          if (y >= top) {
+            $('#sidebar-products').addClass('sub-widget-2');
+        } else {
+            $('#sidebar-products').removeClass('sub-widget-2');
+         }
+     });
+
 	actualizarLote();
 
 	// Cargar archivo
