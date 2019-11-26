0df47804059cd7650e67bb011fc9be37d649c337
epenaloza <epenaloza@novopayment.com>
2019-11-26 03:31:04 -0500

    Incluye pais para Banco Bogotá

--- build/application/config/config-bdb.php
+++ build/application/config/config-bdb.php
@@ -1,6 +1,6 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
 
-$config['country'] = 'Pe';
+$config['country'] = 'Bdb';
 $config['base_url']	= BASE_URL.'bdb/';
 $config['language']	= 'spanish-bdb';
 $config['favicon'] = 'favicon-bdb';
