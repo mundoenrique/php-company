99b912304fd9addb1d25beea36bf2d7caf7b1185
epenaloza <epenaloza@novopayment.com>
2020-02-03 19:33:53 -0400

    cambia valor de para mostrar lista de sucursales

--- build/application/language/spanish-bdb/bulk_lang.php
+++ build/application/language/spanish-bdb/bulk_lang.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang['BULK_GET_BRANCHOFFICE'] = 'B';
+$lang['BULK_GET_BRANCHOFFICE'] = '10';
 $lang['BULK_DELETE'] = 'Por favor ingresa el código de seguridad para eliminar el lote';
 $lang['BULK_DELETE_DATE'] = 'Por favor ingresa el código de seguridad para eliminar el lote de fecha';
