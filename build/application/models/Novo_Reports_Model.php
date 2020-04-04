6cbd10e811c40d0ffd9f856c4dcc1c840a324d7d
epenaloza <epenaloza@novopayment.com>
2020-04-04 19:53:06 -0400

    restaura nombre de atributo

--- build/application/models/Novo_Reports_Model.php
+++ build/application/models/Novo_Reports_Model.php
@@ -30,9 +30,7 @@ class Novo_Reports_Model extends NOVO_Model {
 		$this->dataRequest->idOperation = 'listadoReportesCEO';
 		$this->dataRequest->enterpriseCode = $this->session->enterpriseInf->enterpriseCode;
 		$this->dataRequest->enterpriseGroup = $this->session->enterpriseInf->enterpriseGroup;
-		$this->dataRequest->empresaCliente = [
-			'rif' => $this->session->enterpriseInf->idFiscal
-		];
+		$this->dataRequest->rif = $this->session->enterpriseInf->idFiscal;
 		$this->dataRequest->nombre = $this->session->enterpriseInf->enterpriseName;
 
 		$response = $this->sendToService('GetReportsList');
