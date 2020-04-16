3d3c0b646f1fc1bb8b0f505416604884712da098
Diego Acosta <dacosta@novopayment.onmicrosoft.com>
2020-04-16 12:10:13 -0500

    Footer dynamicc

--- build/application/language/spanish-bnt/general_lang.php
+++ build/application/language/spanish-bnt/general_lang.php
@@ -15,4 +15,7 @@ $lang['GEN_FILE'] = 'Archivos de gestión Conexión Empresas Online';
 $lang['GEN_CEO_USER_MANUAL'] = 'Manual de Usuario Conexión Empresas Online';
 $lang['GEN_GL_USER_MANUAL'] = 'Manual de Usuario Gestor de Lotes';
 //FOOTER
-$lang['GEN_FOTTER_RIGHTS'] = '© Todos los derechos reservados. Banco de Bogotá - ';
+$lang['GEN_FOTTER_RIGHTS'] = '© Todos los derechos reservados. BANORTE - ';
+$lang['GEN_FOTTER_ICON_I'] = 'banorte-blanco.png';
+$lang['GEN_FOTTER_ICON_R'] = 'img-pci_compliance.svg';
+$lang['GEN_FOTTER_MARK'] = 'img-ma.svg';
--- build/application/views/footer_content-core.php
+++ build/application/views/footer_content-core.php
@@ -1,12 +1,16 @@
+
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 <footer class="main-footer">
+	<? if ($countryUri == 'bdb'): ?>
 	<div class="flex pr-2 pr-lg-0">
-		<img src="<?= $this->asset->insertFile($countryUri.'/img-mark.svg'); ?>" alt="Logo Superintendencia">
+	<img  src="<?= $this->asset->insertFile($countryUri.'/'.lang('GEN_FOTTER_MARK')); ?> " alt="Logo Superintendencia">
 	</div>
+	<? endif; ?>
 	<div class="flex flex-auto flex-wrap justify-around items-center">
-		<img class="order-first" src="<?= $this->asset->insertFile($countryUri.'/img-bogota_white.svg'); ?>"
+
+		<img class="order-first" style="height: 100px; width:300px;" src="<?= $this->asset->insertFile($countryUri.'/'.lang('GEN_FOTTER_ICON_I')); ?>"
 			alt="Logo Banco de Bogotá">
-		<img class="order-1" src="<?= $this->asset->insertFile($countryUri.'/img-pci_compliance.svg'); ?>"
+		<img class="order-1"  src="<?= $this->asset->insertFile($countryUri.'/'.lang('GEN_FOTTER_ICON_R')); ?>"
 			alt="Logo PCI">
 		<span class="copyright-footer mt-1 nowrap flex-auto lg-flex-none order-1 order-lg-0 center h6"><?= lang('GEN_FOTTER_RIGHTS'); ?><?= date("Y") ?></span>
 	</div>
