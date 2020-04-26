6496552ef9ed5a6be6bda5875c31b663457f878a
epenaloza <epenaloza@novopayment.com>
2020-04-26 12:36:44 -0400

    Elimina atributo innecesario

--- build/application/models/Novo_User_Model.php
+++ build/application/models/Novo_User_Model.php
@@ -223,7 +223,6 @@ class Novo_User_Model extends NOVO_Model {
 					'countrySess' => $this->config->item('country'),
 					'countryUri' => $this->config->item('country-uri'),
 					'idUsuario' => $response->usuario->idUsuario,
-					'pais' => $this->config->item('country')
 				];
 				$this->session->set_userdata($userData);
 				$this->response->code = 0;
