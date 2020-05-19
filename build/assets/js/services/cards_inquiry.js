a552ec39752bc2510672477acbc2bca7511fff4f
Jennifer Cadiz <jcadiz@novopayment.com>
2020-05-19 17:15:13 -0400

    Mejora estilos para vista de Consulta Tarjetas

--- build/assets/js/services/cards_inquiry.js
+++ build/assets/js/services/cards_inquiry.js
@@ -8,6 +8,17 @@ $(function () {
 		"ordering": false,
 		"responsive": true,
 		"pagingType": "full_numbers",
+		"columnDefs": [
+      {
+        "targets": 0,
+        "className": "select-checkbox",
+				"checkboxes": {"selectRow": true}
+      }
+		],
+    "select": {
+      "style": lang.GEN_TABLE_SELECT_SIGN,
+			selector: ':not(td:nth-child(-n+6))'
+    },
 		"language": dataTableLang
 	});
 });
