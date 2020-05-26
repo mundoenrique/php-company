77b62f31a5a76053d1df72b46497e352795cf550
Diego Acosta <dacosta@novopayment.onmicrosoft.com>
2020-05-26 13:09:06 -0500

    correccion  de variable de lenguaje

--- build/application/language/spanish-bnt/validate_lang.php
+++ build/application/language/spanish-bnt/validate_lang.php
@@ -2,4 +2,4 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 //VALIDATE FORMS
 $lang['VALIDATE_FISCAL_REGISTRY'] = '^["a-z0-9"]{8,9}';
-$lang['VALIDATE_ID_COMPANY'] = 'indica un RFC válido';
+$lang['VALIDATE_ID_COMPANY'] = 'El ID no es válido';
