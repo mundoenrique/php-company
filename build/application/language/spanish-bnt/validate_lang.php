165cc5eb95ecacc697a6af1d54cf8584fe8cb847
Diego Acosta <dacosta@novopayment.onmicrosoft.com>
2020-05-28 12:44:23 -0500

    modificacion de teexto n recuperar contraseña

--- build/application/language/spanish-bnt/validate_lang.php
+++ build/application/language/spanish-bnt/validate_lang.php
@@ -3,3 +3,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 //VALIDATE FORMS
 $lang['VALIDATE_FISCAL_REGISTRY'] = '^["a-z0-9"]{8,9}';
 $lang['VALIDATE_ID_COMPANY'] = 'El ID no es válido';
+$lang['VALIDATE_USERNAME'] = 'Debes indicar tu nombre de usuario';
