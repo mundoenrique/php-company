167b77823c95bbe8f802c09e963d21d00042e243
epenaloza <epenaloza@novopayment.com>
2020-06-17 13:11:14 -0400

    Incluye validación de tipo de login

--- build/application/models/Novo_User_Model.php
+++ build/application/models/Novo_User_Model.php
@@ -82,6 +82,7 @@ class Novo_User_Model extends NOVO_Model {
 					'cl_addr' => $this->encrypt_connect->encode($this->input->ip_address(), $userName, 'REMOTE_ADDR'),
 					'countrySess' => $this->config->item('country'),
 					'countryUri' => $this->config->item('country-uri'),
+					'autoLogin' => 'false',
 					'idUsuario' => $response->usuario->idUsuario,
 					'pais' => $this->config->item('country'),
 					'nombreCompleto' => $fullName,
@@ -226,6 +227,7 @@ class Novo_User_Model extends NOVO_Model {
 					'cl_addr' => $this->encrypt_connect->encode($this->input->ip_address(), $this->country, 'REMOTE_ADDR'),
 					'countrySess' => $this->config->item('country'),
 					'countryUri' => $this->config->item('country-uri'),
+					'autoLogin' => 'true',
 				];
 				$this->session->set_userdata($userData);
 				$this->response->code = 0;
