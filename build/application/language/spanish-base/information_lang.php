db8e19c87531701d83c5992602dcbe2b8e671028
epenaloza <epenaloza@novopayment.com>
2020-07-02 16:04:32 -0400

    Cambia varibles de lenguaje

--- build/application/language/spanish-base/information_lang.php
+++ build/application/language/spanish-base/information_lang.php
@@ -18,3 +18,17 @@ $lang["TERMS_CONTENT"] = "<p><strong>Tebca Perú, Transferencia Electrónica de 
 	<p>La Empresa se reserva a su total discreción, emplear todos los medios necesarios a su alcance para retirar de forma inmediata al usuario que infrinja cualquiera de las condiciones de seguridad  y privacidad de La Plataforma , lo cual incluye que los Usuarios hayan enviado información falsa que no sea susceptible de verificarla en cuanto a su autenticidad.</p>
 	<p>Igualmente, La Empresa, tiene el derecho de cambiar las políticas de seguridad y privacidad, siendo que todo cambio será publicado en La Plataforma  a los fines de notificar a sus Usuarios.</p>
 	<p>Así pues, en caso de cualquier violación a las condiciones de seguridad y privacidad, a los sistemas que utilizan tecnología de la información, a la privacidad de las personas y comunicaciones y a la propiedad intelectual que estén vinculados a La Plataforma , se le aplicará lo dispuesto en el Código Penal sobre Delitos Informáticos y demás normas vigentes y aplicables del ordenamiento jurídico del Perú.</p>";
+	//BENEFITS
+$lang['TITLE_BENEFITS'] = 'Beneficios';
+$lang['BENEFITS_TITULO_1'] = 'Operaciones seguras';
+$lang['BENEFITS_MSG_1'] = 'Resguardamos la integridad y la privacidad de la información cumpliendo los más altos estándares internacionales de seguridad.';
+$lang['BENEFITS_TITULO_2'] = 'Accesibilidad 7x24';
+$lang['BENEFITS_MSG_2'] = 'Nuestra plataforma está disponible las 24 horas del día, los 7 días de la semana y le ofrece conexión directa vía Internet para emitir y recargar sus tarjetas.';
+$lang['BENEFITS_TITULO_3'] = 'Actualización automática';
+$lang['BENEFITS_MSG_3'] = 'Las actualizaciones y mejoras de la plataforma se ejecutarán de forma automática, permitiéndole a su empresa estar siempre al día con las nuevas funcionalidades que se desarrollen.';
+$lang['BENEFITS_TITULO_4'] = 'Reportes Online';
+$lang['BENEFITS_MSG_4'] = 'Obtenga información sobre las operaciones realizadas y gestione en línea la emisión de reportes y gráficos que facilitan auditorías y controles sobre los gastos realizados por su empresa.';
+$lang['BENEFITS_TITULO_5'] = 'Operaciones';
+$lang['BENEFITS_MSG_5'] = 'Consulte, autorice o anule Lotes de Emisión y Recarga de tarjetas con mayor facilidad y obtenga la Orden de Servicio para facilitar su pago.';
+$lang['BENEFITS_TITULO_6'] = 'Integración';
+$lang['BENEFITS_MSG_6'] = 'Conexión con el sistema administrativo de la empresa (ERP) para facilitar los procesos operativos y contables.';
