abe9d70262b113cc7ecb8e3a98527759c59d62ab
Luis Molina <lmolina@novopayment.com>
2020-07-08 10:41:38 -0430

    Actualiza code de rc -229

--- build/application/models/Novo_User_Model.php
+++ build/application/models/Novo_User_Model.php
@@ -139,7 +139,7 @@ class Novo_User_Model extends NOVO_Model {
 				$this->response->position = lang('CONF_VALID_POSITION');
 				break;
 			case -229:
-				$this->response->code = 2;
+				$this->response->code = 3;
 				$this->response->msg = lang('RESP_OLD_USER');
 				break;
 			case -262:
