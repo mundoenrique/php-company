cf1c508051e38dcba1df7ea14b79cabb7e5b3734
epenaloza <epenaloza@novopayment.com>
2020-07-23 20:17:21 -0400

    Cambia varibles de texto

--- build/application/language/spanish-bdb/general_lang.php
+++ build/application/language/spanish-bdb/general_lang.php
@@ -5,7 +5,7 @@ $lang['GEN_LOGO_HEADER'] = 'img-logo.svg';
 $lang['GEN-LOGO-BIG'] = 'img-banco_bogota.svg';
 $lang['GEN_ALTERNATIVE_TEXT'] = 'Banco de Bogotá';
 $lang['GEN_FISCAL_REGISTRY'] = 'Identificación';
-$lang['GEN_PLACE_PASSWORD'] = 'Código de seguridad';
+$lang['GEN_PLACE_PASSWORD'] = 'token';
 //BUTTONS
 $lang['GEN_BTN_DISASS_AUTH'] = 'En espera de autorización';
 $lang['GEN_BTN_AUTHORIZE'] = 'Aceptar';
@@ -21,5 +21,5 @@ $lang['GEN_FOTTER_RIGHTS'] = '© Todos los derechos reservados. Banco de Bogotá
 $lang['GEN_FOTTER_IMAGE_L'] = 'img-bogota_white.svg';
 $lang['GEN_FOTTER_IMAGE_R'] = 'img-pci_compliance.svg';
 $lang['GEN_FOTTER_MARK'] = 'img-mark.svg';
-$lang['RESP_PASSWORD_NO_VALID'] = 'Por favor verifica el código de seguridad y vuelve a intentarlo';
-$lang['BULK_DELETE_SO'] = 'Por favor ingresa el código de seguridad para anular la orden de servicio: ';
+$lang['RESP_PASSWORD_NO_VALID'] = 'Por favor verifica el token y vuelve a intentarlo';
+$lang['BULK_DELETE_SO'] = 'Por favor ingresa el token para anular la orden de servicio: ';
