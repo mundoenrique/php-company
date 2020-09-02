a760244875dbc75690290283dfb2e873e45ee26f
epenaloza <epenaloza@novopayment.com>
2020-09-02 18:53:27 -0400

    Incluye validación de url cliente

--- build/application/core/NOVO_Controller.php
+++ build/application/core/NOVO_Controller.php
@@ -63,6 +63,7 @@ class NOVO_Controller extends CI_Controller {
 		languageLoad('specific', $this->router->fetch_class());
 		if($this->session->has_userdata('userId')) {
 			if($this->session->countrySess !== $this->config->item('country')) {
+				clientUrlValidate($this->session->countrySess);
 				$urlRedirect = str_replace($this->countryUri.'/', $this->session->countryUri.'/', base_url('cerrar-sesion/inicio'));
 				redirect($urlRedirect, 'location', 301);
 				exit();
@@ -191,13 +192,9 @@ class NOVO_Controller extends CI_Controller {
 			}
 
 		} else {
-			/* $linkredirect = $this->session->has_userdata('productInf') ? 'detalle-producto' : 'empresas';
-			$linkredirect = !$this->session->has_userdata('logged') ? 'inicio' : $linkredirect;
-			$linkredirect = AUTO_LOGIN ? 'ingresar/fin' : $linkredirect; */
 			$linkredirect = uriRedirect(AUTO_LOGIN);
 			redirect(base_url($linkredirect), 'location');
 		}
-
 	}
 	/**
 	 * Método para cargar un modelo especifico
