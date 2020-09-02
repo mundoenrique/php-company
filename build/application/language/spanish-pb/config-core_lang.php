58d2629e39b1d0d92012124e2b348ea4eb778f47
Jhonnatan Vega <jvega@novopayment.com>
2020-09-02 09:51:57 -0400

    Incorpora archivos de Descarga en Configuración

--- build/application/language/spanish-pb/config-core_lang.php
+++ build/application/language/spanish-pb/config-core_lang.php
@@ -15,3 +15,10 @@ $lang['CONF_SETTINGS_EMAIL_UPDATE'] = 'OFF';
 $lang['CONF_SETTINGS_BRANCHES'] = 'OFF';
 $lang['CONF_SETTINGS_PHONES_UPDATE'] = 'OFF';
 $lang['CONF_SETTINGS_CONTACT'] = 'OFF';
+//FILES CONF
+$lang['CONF_MANUAL_FILE'] = [
+	['archivo.pdf', 'Manual']
+];
+$lang['CONF_FILES_MANAGMENT'] = [
+  ['lotes.rar', 'Gestión de Lotes']
+];
