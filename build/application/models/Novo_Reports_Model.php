bb33cce4b9c1a8a62bc7c4321bd5dc768ce12644
Diego Acosta <dacosta@novopayment.com>
2020-09-07 14:05:20 -0500

    convierte a mayusculas input CURP en estado de cuenta y cuenta maestra

--- build/application/models/Novo_Reports_Model.php
+++ build/application/models/Novo_Reports_Model.php
@@ -1307,8 +1307,11 @@ class Novo_Reports_Model extends NOVO_Model {
 		}else {
 			$typeSearch = '1';
 		}
-		$this->dataRequest->idExtEmp = $dataRequest->enterpriseCode;
 		$this->dataRequest->idExtPer = $dataRequest->resultByNIT;
+		if(lang('CONF_INPUT_UPPERCASE') == 'OFF'){
+			$this->dataRequest->idExtPer = strtoupper($dataRequest->resultByNIT);
+		}
+		$this->dataRequest->idExtEmp = $dataRequest->enterpriseCode;
     $lastDayMonyh = date("t-m-Y", strtotime(str_replace( '/', '-', "1/".$dataRequest->initialDateAct)));
 		$this->dataRequest->fechaFin = str_replace( '-', '/', $lastDayMonyh);
 		$this->dataRequest->fechaIni = "1/".$dataRequest->initialDateAct;
