db7e6c6260ebb4cba9cced6eaedf3f36ad1f2e70
epenaloza <epenaloza@novopayment.com>
2020-10-21 13:45:11 -0400

    Incluye password en de sesión para SingleSignOn

--- build/application/models/Novo_User_Model.php
+++ build/application/models/Novo_User_Model.php
@@ -298,6 +298,7 @@ class Novo_User_Model extends NOVO_Model {
 					'logged' => TRUE,
 					'userId' => $response->usuario->idUsuario,
 					'userName' => $response->usuario->userName,
+					'passWord' => $response->usuario->password ?? FALSE,
 					'fullName' => ucwords(mb_strtolower($fullName)),
 					'codigoGrupo' => $response->usuario->codigoGrupo,
 					'lastSession' => $lastSession,
