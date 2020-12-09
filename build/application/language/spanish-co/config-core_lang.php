0c46ca804cd1b523525492e57241b49a14308893
epenaloza <epenaloza@novopayment.com>
2020-12-09 11:03:59 -0400

    Valida inputs

--- build/application/language/spanish-co/config-core_lang.php
+++ build/application/language/spanish-co/config-core_lang.php
@@ -2,5 +2,3 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 //SUPPORT IE 11
 $lang['CONF_SUPPORT_IE'] = 'ON';
-//SERVICES TRANSFER MASTER ACCOUNT
-$lang['CONF_SELECT_AMOUNT'] = 'OFF';
