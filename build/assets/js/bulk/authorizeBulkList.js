a0e10e95f99ec23f8d25f377a30e57fb2e3b73d2
Diego Acosta <dacosta@novopayment.com>
2021-02-15 15:05:36 -0600

    correccion de solucion para boton X en autorizacion de lotes

--- build/assets/js/bulk/authorizeBulkList.js
+++ build/assets/js/bulk/authorizeBulkList.js
@@ -143,8 +143,7 @@ $(function () {
 				var oldId = $('#accept').attr('id');
 				var currentIdBtn = 'delete-bulk-btn';
 				var cancelDelete = $('#cancel');
-				$('#accept').attr('id', currentIdBtn);
-				$('#delete-bulk-btn').show();
+				$('#accept').show().attr('id', currentIdBtn);
 				var bulkNum = $(this).closest('tr').find('.bulk-num').text();
 				modalReq['table'] = $(this).closest('table');
 				modalBtn = {
