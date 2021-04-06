0fa895bd8f6df73a700d02d237cee914939fe468
Diego Acosta <dacosta@novopayment.com>
2021-04-06 13:58:06 -0500

    Modificacion de textos en portal para Banorte

--- build/application/language/spanish-bnt/services_lang.php
+++ build/application/language/spanish-bnt/services_lang.php
@@ -1,6 +1,16 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang['SERVICES_CARD_TRANSFER_BALANCE'] = 'No fue posible transferir el saldo a la tarjeta destino.';
-$lang['SERVICES_TRANSACTION_FAIL'] = 'No fue posible realizar la trasacción.';
+$lang['SERVICES_CARD_TRANSFER_BALANCE'] = 'No fue posible transferir el saldo a la tarjeta destino, selecciona otra tarjeta';
+$lang['SERVICES_TRANSACTION_FAIL'] = 'No fue posible realizar la trasacción, intenta de nuevo';
 $lang['SERVICES_UNAVAILABLE_BALANCE'] = 'No se obtuvo información del saldo de la cuenta maestra.';
+$lang['SERVICES_TABLE_NO_REGISTRY_MASTERACCOUNT'] = 'El listado de tarjetas no se encuentra disponible. verifica si tienes tarjetas afiliadas, ó realiza la búsqueda de nuevo';
+$lang['SERVICES_BALANCE_NO_AVAILABLE'] = 'El saldo de la cuenta maestra no está disponible, verifícalo';
+$lang['SERVICES_NO_BALANCE'] = 'Tu saldo no es suficiente para realizar la transacción, consulta el saldo';
+$lang['SERVICES_BLOCKED_CARD'] = 'La tarjeta %s ya se encuentra bloqueda, selecciona otra tarjeta';
+$lang['SERVICES_CARD_BULK_AFFILIATED'] = 'La tarjeta %s ya fue afiliada o esta por afiliar en un lote confirmado, selecciona otra tarjeta';
+$lang['SERVICES_NO_FOUND_REGISTRY'] = 'El número de tarjeta <strong>%s</strong> no se ha encontrado, verifica';
+$lang['SERVICES_TWIRLS_NO_AVAILABLE_CARD'] = 'El número de tarjeta <strong>%s</strong> no está disponible, verifica';
+$lang['SERVICES_TWIRLS_EXPIRED_CARD'] = 'La tarjeta <strong>%s</strong> está vencida, asigna una nueva';
+$lang['SERVICES_TWIRLS_TEMPORARY_BLOCKED_CARD'] = 'El número de tarjeta <strong>%s</strong> está bloqueada temporalmente, para consultar desbloquéala y despúes de tu consulta bloquéala';
+$lang['SERVICES_LIMITS_NO_UPDATE'] = 'No fue posible actualizar la restricción de límites, intenta de nuevo';
