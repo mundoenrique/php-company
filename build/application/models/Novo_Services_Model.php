fdddcf74abc4e1796ca7d311dbbb53fc71e4169f
Diego Acosta <dacosta@novopayment.com>
2021-05-14 13:01:47 -0500

    Agrega mensaje personalizado para rc -33 de busqueda de saldo en cuenta maestra

--- build/application/models/Novo_Services_Model.php
+++ build/application/models/Novo_Services_Model.php
@@ -311,6 +311,10 @@ class Novo_Services_Model extends NOVO_Model {
 				$this->response->modalBtn['btn1']['action'] = 'destroy';
 			break;
 			case -33:
+				$this->response->title = lang('GEN_'.$dataRequest->action);
+				$this->response->msg = lang('SERVICES_BALANCE_NO_SEARCH');
+				$this->response->icon = lang('CONF_ICON_WARNING');
+				$this->response->modalBtn['btn1']['action'] = 'destroy';
 			case -100:
 				$this->response->title = lang('GEN_'.$dataRequest->action);
 				$this->response->msg = lang('SERVICES_BALANCE_NO_AVAILABLE');
