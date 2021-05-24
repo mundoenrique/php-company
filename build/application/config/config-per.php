4aa1d21c7a6480fa1c8c72f35d31bae89a10adb9
epenaloza <epenaloza@novopayment.com>
2021-05-24 12:39:26 -0400

    Incluye nuevos config

---
+++ build/application/config/config-per.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+$config['customer'] = 'Pe';
+$config['customer-uri'] = 'per';
+$config['base_url']	= BASE_URL.$config['customer-uri'].'/';
+$config['language']	= BASE_LANGUAGE.'-'.$config['customer-uri'];
---
+++ build/application/config/config-ven.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+$config['customer'] = 'Ve';
+$config['customer-uri'] = 'ven';
+$config['base_url']	= BASE_URL.$config['customer-uri'].'/';
+$config['language']	= BASE_LANGUAGE.'-'.$config['customer-uri'];
