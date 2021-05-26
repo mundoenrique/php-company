349b9d346782563ba1431dff489322f0feb6f6a7
Luis Molina <lmolina@novopayment.com>
2021-05-26 13:56:20 -0400

    Agrega variables de configuración modulo recarga cuenta maestra

--- build/application/language/global/config-core-bpi_lang.php
+++ build/application/language/global/config-core-bpi_lang.php
@@ -1,2 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+
+//SERVICES TRANSFER MASTER ACCOUNT RECHARGE
+$lang['CONF_SELECT_TYPE'] = 'ON';
+$lang['CONF_INPUT_PASS'] = 'ON';
+$lang['CONF_VALIDATE_PARAMS'] = 'ON';
