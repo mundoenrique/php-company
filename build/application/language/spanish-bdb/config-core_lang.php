e5712c2e5b88325b8d17e5568de7a936015857c6
Luis Molina <lmolina@novopayment.com>
2021-07-06 15:28:48 -0400

    Actualiza archivo de descarga a lotes.rar

--- build/application/language/spanish-bdb/config-core_lang.php
+++ build/application/language/spanish-bdb/config-core_lang.php
@@ -35,7 +35,7 @@ $lang['CONF_MANUAL_FILE'] = [
   ['archivo.pdf', 'Manual de Usuario Conexión Empresas Online']
 ];
 $lang['CONF_FILES_MANAGMENT'] = [
-  ['archivos_autogestion_ceo.rar', 'Archivos de autogestión']
+  ['lotes.rar', 'Archivos de autogestión']
 ];
 $lang['CONF_APPS_DOWNLOAD'] = [
   ['download-ini', 'Archivo.ini']
