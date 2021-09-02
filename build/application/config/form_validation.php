593355e2a1e73e27a2d265c598d54efc037bce07
epenaloza <epenaloza@novopayment.com>
2021-09-02 16:14:18 -0400

    Incorpora regla de validación

--- build/application/config/form_validation.php
+++ build/application/config/form_validation.php
@@ -1603,6 +1603,12 @@ $config = [
 			'rules' => 'trim'
 		]
 	],
+	'changeLanguage' => [
+		[
+			'field' => 'lang',
+			'rules' => 'trim|alpha|regex_match[/es|en/]|required'
+		]
+	],
 	// Old Arquitectures
 	'login' => [
 		[
