e74185e8a37e40bc65f68e4249db05a84d82c2f3
jcadiz <jcadiz@novopayment.com>
2022-05-20 10:44:54 -0400

    Actualiza longitud de nombre de usuario

--- build/application/views/widget/widget_menu-user_content-core.php
+++ build/application/views/widget/widget_menu-user_content-core.php
@@ -2,8 +2,9 @@
 <nav class="main-nav main-nav-dropdown">
   <ul class="flex my-0 items-center list-style-none list-inline">
     <li>
-      <a class="mt-1 mx-1 regular text-decoration-none white" href="#"><?= $fullName ?>
-        <i class="ml-5 icon icon-chevron-down" aria-hidden="true"></i>
+      <a class="mt-1 mx-1 regular text-decoration-none white flex" href="#">
+				<span class="inline-block tool-ellipsis"><?= $fullName ?></span>
+        <i class="ml-3 icon icon-chevron-down" aria-hidden="true"></i>
       </a>
       <ul class="dropdown regular tertiary bg-secondary">
         <?php if(lang('CONF_SETT_CONFIG') == 'ON'): ?>
