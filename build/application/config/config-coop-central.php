4dec4d6ac46a81e6b6612a6600bd5add14d12966
epenaloza <epenaloza@novopayment.com>
2022-07-14 07:38:26 -0400

    Habilita tenat provisional

---
+++ build/application/config/config-coop-central.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+$config['customer'] = 'coop';
+$config['customer-uri'] = 'coop-central';
+$config['base_url']	= BASE_URL.$config['customer-uri'].'/';
+$config['language']	= BASE_LANGUAGE.'-'.$config['customer-uri'];
