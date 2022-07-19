efb1e42d7b2c3e9d7f2dbdbf85c4cbb6f48117d6
ylopez <ylopez@novopayment.com>
2022-07-19 18:02:39 -0500

    Incorpora configuraciones

--- build/application/language/global/config-core-coop_lang.php
+++ build/application/language/global/config-core-coop_lang.php
@@ -9,3 +9,14 @@ $lang['CONF_SETTINGS_CONTACT'] = 'OFF';
 $lang['CONF_WIDGET_REST_COUNTRY'] = 'ON';
 //VALIDATE FORMS
 $lang['CONF_VALIDATE_FISCAL_REGISTRY'] = '^([0-9]{9,17})';
+//FOOTER
+$lang['CONF_SUPERINTENDENCY_LOGO'] = 'ON';
+$lang['CONF_FOOTER_NETWORKS'] = 'ON';
+$lang['CONF_FOOTER_LOGO'] = 'OFF';
+//EXTERNAL LINKS
+$lang['CONF_FOTTER_NETWORKS_LINK'] = [
+	'facebook' => 'https://www.facebook.com/BCCoopcentral',
+	'twitter' => 'https://www.twitter.com/BcoCoopcentral',
+	'instagram' => 'https://www.instagram.com/bccoopcentral/?hl=es-la',
+	'linkedin' => 'https://www.linkedin.com/company/banco-cooperativo-coopcentral'
+];
