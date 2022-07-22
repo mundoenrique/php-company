9887fe53f33c92c57aae5a33dbfbbb7c8c3765b0
ylopez <ylopez@novopayment.com>
2022-07-22 10:10:52 -0500

    Incorpora config y estilos coop-central

---
+++ build/application/language/global/config-core-coop-central_lang.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+//SETTINGS
+$lang['CONF_SETTINGS_EMAIL_UPDATE'] = 'OFF';
+$lang['CONF_SETTINGS_BRANCHES'] = 'OFF';
+$lang['CONF_SETTINGS_PHONES_UPDATE'] = 'OFF';
+$lang['CONF_SETTINGS_CONTACT'] = 'OFF';
+//SIGNIN
+$lang['CONF_WIDGET_REST_COUNTRY'] = 'ON';
+//VALIDATE FORMS
+$lang['CONF_VALIDATE_FISCAL_REGISTRY'] = '^([0-9]{9,17})';
+//FOOTER
+$lang['CONF_SUPERINTENDENCY_LOGO'] = 'ON';
+$lang['CONF_FOOTER_NETWORKS'] = 'ON';
+$lang['CONF_FOOTER_LOGO'] = 'OFF';
+//EXTERNAL LINKS
+$lang['CONF_FOTTER_NETWORKS_LINK'] = [
+	'facebook' => 'https://www.facebook.com/BCCoopcentral',
+	'twitter' => 'https://www.twitter.com/BcoCoopcentral',
+	'instagram' => 'https://www.instagram.com/bccoopcentral/?hl=es-la',
+	'linkedin' => 'https://www.linkedin.com/company/banco-cooperativo-coopcentral'
+];
---
+++ build/application/language/global/images_coop-central_lang.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['GEN_FAVICON'] = 'favicon-coop';
+$lang['GEN-LOGO-BIG'] = 'img-coopcentral-white.svg';
+$lang['GEN_FOTTER_IMAGE_L'] = 'img-coopcentral-white.svg';
+$lang['GEN_FOTTER_MARK'] = 'img-mark.svg';
+// PROGRAM IMAGES
+$lang['IMG_PROGRAM_IMAGES'] = [
+	'efectiva' => 'efectiva',
+];
+$lang['GEN_FOTTER_NETWORKS_IMG'] = [
+	'facebook' => 'facebook.png',
+	'twitter' => 'twitter.png',
+	'instagram' => 'instagram.png',
+	'linkedin' => 'linkedin.png'
+];
