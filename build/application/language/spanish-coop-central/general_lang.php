c0eac58e8028f6d7ba7fc01e09f9dfe2fc70eb12
ylopez <ylopez@novopayment.com>
2022-07-29 09:30:03 -0500

    Incorpora textos coop-central

---
+++ build/application/language/spanish-coop-central/general_lang.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['GEN_CONTACTS_CITY'] = 'CIUDAD';
+$lang['GEN_CONTACTS_CONTACT'] = 'CONTACTO';
+$lang['GEN_CONTACTS'] = [
+	'XXXXX' => 'XXXXXXX',
+];
+$lang['GEN_FOTTER_RIGHTS'] = '© Banco Cooperativo Coopcentral - ';
