b16e9bba5c6c55ca7b89e97ca4e74d3c8ee1ce88
ylopez <ylopez@novopayment.com>
2022-08-10 14:48:37 -0500

    Agrega variable GEN_FISCAL_REGISTRY

--- build/application/language/spanish-coop-central/general_lang.php
+++ build/application/language/spanish-coop-central/general_lang.php
@@ -3,6 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $lang['GEN_CONTACTS_CITY'] = 'CIUDAD';
 $lang['GEN_CONTACTS_CONTACT'] = 'CONTACTO';
+$lang['GEN_FISCAL_REGISTRY'] = 'NIT';
 $lang['GEN_CONTACTS'] = [
 	'XXXXX' => 'XXXXXXX',
 ];
