428901a9a25b3425b1f51f54a485e80d7a21cfa1
ylopez <ylopez@novopayment.com>
2022-09-01 16:11:23 -0500

    Actualiza imagen logo.

--- build/application/language/global/images_coop-central_lang.php
+++ build/application/language/global/images_coop-central_lang.php
@@ -2,8 +2,8 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $lang['GEN_FAVICON'] = 'favicon-coop';
-$lang['GEN-LOGO-BIG'] = 'img-coopcentral-white.svg';
-$lang['GEN_FOTTER_IMAGE_L'] = 'img-coopcentral-white.svg';
+$lang['GEN-LOGO-BIG'] = 'img-coopcentral-white.png';
+$lang['GEN_LOGO_HEADER'] = 'img-coopcentral-white.png';
 $lang['GEN_FOTTER_MARK'] = 'img-mark.svg';
 // PROGRAM IMAGES
 $lang['IMG_PROGRAM_IMAGES'] = [
