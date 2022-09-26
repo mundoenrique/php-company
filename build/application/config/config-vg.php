c1731f7c4728feab185f8bf340673a78221ac76d
ylopez <ylopez@novopayment.com>
2022-09-26 11:17:12 -0500

    Incorpora variable client_style en voygo

--- build/application/config/config-vg.php
+++ build/application/config/config-vg.php
@@ -2,6 +2,7 @@
 
 $config['customer'] = 'Usd';
 $config['customer-uri'] = 'vg';
+$config['client_style'] = $config['customer-uri'];
 $config['base_url']	= BASE_URL.$config['customer-uri'].'/';
 $config['language']	= BASE_LANGUAGE.'-'.$config['customer-uri'];
 $config['channel'] = 'voygoBusiness';
