38cd3815c70e068a1d48e0b51757c913b9c6090a
lmolina <lmolina@novopayment.com>
2022-11-08 11:18:46 -0400

    Revert "Agrega nuevos RC COOPCENTRAL"
    This reverts commit 26aec0712eb0a7a692f61c23cc2090b201bf378c.

--- build/application/models/Novo_Inquiries_Model.php
+++ build/application/models/Novo_Inquiries_Model.php
@@ -684,39 +684,14 @@ class Novo_Inquiries_Model extends NOVO_Model {
 				$this->session->unset_userdata('requestOrdersList');
 				$this->session->unset_userdata('authToken');
 			break;
-			case -14:
-				$this->response->icon = lang('CONF_ICON_WARNING');
-				$this->response->msg = lang('PAG_MONTHLY_AMOUNT_MAX_EXCEEDED');
-				$this->response->modalBtn['btn1']['action'] = 'destroy';
-			break;
 			case -21:
 				$this->response->icon = lang('CONF_ICON_WARNING');
 				$this->response->msg = lang('PAGO_OS_CONECTION_ERROR');
 				$this->response->modalBtn['btn1']['action'] = 'destroy';
 			break;
-			case -24:
-				$this->response->icon = lang('CONF_ICON_WARNING');
-				$this->response->msg = lang('PAG_DAILY_TRANSACTION_EXCEEDED');
-				$this->response->modalBtn['btn1']['action'] = 'destroy';
-			break;
-			case -25:
-				$this->response->icon = lang('CONF_ICON_WARNING');
-				$this->response->msg = lang('PAG_DAILY_AMOUNT_EXCEEDED');
-				$this->response->modalBtn['btn1']['action'] = 'destroy';
-			break;
 			case -155:
 				$this->response->icon = lang('CONF_ICON_WARNING');
 				$this->response->msg = lang('PAGO_OS_UNAVAILABLE_BALANCE');
-				$this->response->modalBtn['btn1']['action'] = 'destroy';
-			break;
-			case -220:
-			case -275:
-			case -468:
-			case -470:
-			case -471:
-			case -474:
-				$this->response->icon = lang('CONF_ICON_WARNING');
-				$this->response->msg = lang('PAG_ACCOUNT_NOT_AVAILABLE');
 				$this->response->modalBtn['btn1']['action'] = 'destroy';
 			break;
 			case -230:
@@ -786,36 +761,6 @@ class Novo_Inquiries_Model extends NOVO_Model {
 			case -300:
 				$this->response->icon = lang('CONF_ICON_WARNING');
 				$this->response->msg = $response->msg;
-				$this->response->modalBtn['btn1']['action'] = 'destroy';
-			break;
-			case -472:
-				$this->response->icon = lang('CONF_ICON_WARNING');
-				$this->response->msg = lang('PAG_INVALID_DOCUMENT');
-				$this->response->modalBtn['btn1']['action'] = 'destroy';
-			break;
-			case -473:
-				$this->response->icon = lang('CONF_ICON_WARNING');
-				$this->response->msg = lang('PAG_MONTHLY_AMOUNT_EXCEEDED');
-				$this->response->modalBtn['btn1']['action'] = 'destroy';
-			break;
-			case -475:
-				$this->response->icon = lang('CONF_ICON_WARNING');
-				$this->response->msg = lang('PAG_CONSIGNMENT_AMOUNT_EXCEEDED');
-				$this->response->modalBtn['btn1']['action'] = 'destroy';
-			break;
-			case -476:
-				$this->response->icon = lang('CONF_ICON_WARNING');
-				$this->response->msg = lang('PAG_DEBITS_AMOUNT_MAX_EXCEEDED');
-				$this->response->modalBtn['btn1']['action'] = 'destroy';
-			break;
-			case -477:
-				$this->response->icon = lang('CONF_ICON_WARNING');
-				$this->response->msg = lang('PAG_DEBITS_CONSIGNMENT_AMOUNT_MAX_EXCEEDED');
-				$this->response->modalBtn['btn1']['action'] = 'destroy';
-			break;
-			case -478:
-				$this->response->icon = lang('CONF_ICON_WARNING');
-				$this->response->msg = lang('PAG_AMOUNT_MAX_EXCEEDED');
 				$this->response->modalBtn['btn1']['action'] = 'destroy';
 			break;
 		}
