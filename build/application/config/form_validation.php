e671033e26425c7043710288730afbe551f12096
ylopez <ylopez@novopayment.com>
2022-11-11 08:45:15 -0500

    Ajusta validación email.

--- build/application/config/form_validation.php
+++ build/application/config/form_validation.php
@@ -57,7 +57,7 @@ $config = [
 		[
 			'field' => 'email',
 			'label' => 'email',
-			'rules' => 'trim|regex_match[/^([a-zA-Z0-9_.+\-]*)+\@(([a-zA-Z0-9_\-])+\.)+([a-zA-Z0-9]{2,4})+$/]|required'
+			'rules' => 'trim|regex_match[/^([a-zA-Z0-9]*)+\@(([a-zA-Z0-9_\-])+\.)+([a-zA-Z0-9]{2,4})+$/]|required'
 		]
 	],
 	'recoverAccess' => [
@@ -74,7 +74,7 @@ $config = [
 		[
 			'field' => 'email',
 			'label' => 'email',
-			'rules' => 'trim|regex_match[/^([a-zA-Z0-9_.+\-]*)+\@(([a-zA-Z0-9_\-])+\.)+([a-zA-Z0-9]{2,4})+$/]|required'
+			'rules' => 'trim|regex_match[/^([a-zA-Z0-9]*)+\@(([a-zA-Z0-9_\-])+\.)+([a-zA-Z0-9]{2,4})+$/]|required'
 		]
 	],
 	'validateOtp' => [
@@ -105,7 +105,7 @@ $config = [
 		[
 			'field' => 'email',
 			'label' => 'email',
-			'rules' => 'trim|regex_match[/^([a-zA-Z0-9_.+\-]*)+\@(([a-zA-Z0-9_\-])+\.)+([a-zA-Z0-9]{2,4})+$/]|required'
+			'rules' => 'trim|regex_match[/^([a-zA-Z0-9]*)+\@(([a-zA-Z0-9_\-])+\.)+([a-zA-Z0-9]{2,4})+$/]|required'
 		]
 	],
 	'closingBudgets' => [
@@ -477,7 +477,7 @@ $config = [
 		[
 			'field' => 'emailModifyContact',
 			'label' => 'emailModifyContact',
-			'rules' => 'trim|regex_match[/^([a-zA-Z0-9_.+\-]*)+\@(([a-zA-Z0-9_\-])+\.)+([a-zA-Z0-9]{2,4})+$/]|required'
+			'rules' => 'trim|regex_match[/^([a-zA-Z0-9]*)+\@(([a-zA-Z0-9_\-])+\.)+([a-zA-Z0-9]{2,4})+$/]|required'
 		],
 		[
 			'field' => 'modifyContactPass',
@@ -514,7 +514,7 @@ $config = [
 		[
 			'field' => 'emailNewContact',
 			'label' => 'emailNewContact',
-			'rules' => 'trim|regex_match[/^([a-zA-Z0-9_.+\-]*)+\@(([a-zA-Z0-9_\-])+\.)+([a-zA-Z0-9]{2,4})+$/]|required'
+			'rules' => 'trim|regex_match[/^([a-zA-Z0-9]*)+\@(([a-zA-Z0-9_\-])+\.)+([a-zA-Z0-9]{2,4})+$/]|required'
 		],
 		[
 			'field' => 'newContPass',
