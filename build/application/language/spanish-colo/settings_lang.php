1e7572739d388ba7859c21f5bc303a3b0d2c261e
Enrique Peñaloza <epenaloza@novopayment.com>
2023-05-19 11:12:36 -0400

    Cambia ombre de archivos y variables

--- build/application/language/spanish-colo/settings_lang.php
+++ build/application/language/spanish-colo/settings_lang.php
@@ -1,7 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 $lang['SETT_VIEW_SUFFIX'] = '';
-//VALIDATE FORMS
 $lang['SETT_SIGIN_RECOVER_PASS'] = 'OFF';
 $lang['SETT_MAINTENANCE'] = 'ON';
 $lang['SETT_VALID_ERROR'] = 'validate-error';
@@ -12,12 +11,9 @@ $lang['SETT_VALID_ELEMENT'] = 'label';
 $lang['SETT_VALID_INVALID_USER'] = 'error-login-2';
 $lang['SETT_VALID_INACTIVE_USER'] = 'login-inactive';
 $lang['SETT_VALID_POSITION'] = 'left';
-//MODAL STYLES
 $lang['SETT_MODAL_WIDTH'] = 310;
-//EXTERNAL LINKS
 $lang['SETT_LINK_SIGNIN'] = 'inicio';
 $lang['SETT_LINK_CHANGE_PASS'] = 'cambiar-clave';
 $lang['SETT_LINK_ENTERPRISES'] = 'dashboard';
 $lang['SETT_LINK_TERMS'] = 'inf-condiciones';
-//INTERNAL LINKS
 $lang['SETT_LINK_SUGGESTION'] = 'browsers';
