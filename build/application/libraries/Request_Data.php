d2a06c9c9b16fcf1f94efdca58ce5eca05e48a66
Enrique Peñaloza <epenaloza@novopayment.com>
2023-07-24 10:21:47 -0400

    Añade letra faltante en filtro de empresas

--- build/application/libraries/Request_Data.php
+++ build/application/libraries/Request_Data.php
@@ -165,7 +165,7 @@ class Request_Data {
 				}
 			}
 
-			if(strpos('ZX', $TemCategory) !== FALSE) {
+			if(strpos('XYZ', $TemCategory) !== FALSE) {
 				if($itemAlphabetic7 > $enterpriseArgs->sizePage) {
 					$itemAlphabetic7 = 1; $pageAlphabetic7++;
 				}
