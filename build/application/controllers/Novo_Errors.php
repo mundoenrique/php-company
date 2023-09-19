29d3fdea35015ad62b98ec2f079e8130cdd31c24
Enrique Peñaloza <epenaloza@novopayment.com>
2023-09-19 00:54:21 -0400

    Respuesta para petición asincrona

--- build/application/controllers/Novo_Errors.php
+++ build/application/controllers/Novo_Errors.php
@@ -1,15 +1,16 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 /**
  * @info Controlador para el manejo de errores
  * @author J. Enrique Peñaloza Piñero
  * @date July 13th, 2021
-*/
-class Novo_Errors extends NOVO_Controller {
+ */
+class Novo_Errors extends NOVO_Controller
+{
 
 	public function __construct()
 	{
-		parent:: __construct();
+		parent::__construct();
 		writeLog('INFO', 'Novo_Errors Controller Class Initialized');
 
 		$this->config->set_item('language', BASE_LANGUAGE . '-base');
@@ -18,7 +19,7 @@ class Novo_Errors extends NOVO_Controller {
 		$this->config->set_item('language', $customerLang);
 		$pathLang = APPPATH . 'language' . DIRECTORY_SEPARATOR . $customerLang . DIRECTORY_SEPARATOR;
 
-		if(file_exists($pathLang . 'errors_lang.php')) {
+		if (file_exists($pathLang . 'errors_lang.php')) {
 			$this->lang->load('errors');
 		}
 	}
@@ -31,10 +32,14 @@ class Novo_Errors extends NOVO_Controller {
 	{
 		writeLog('INFO', 'errors: pageNoFound Method Initialized');
 
+		if ($this->input->is_ajax_request()) {
+			$this->output->set_content_type('application/json')->set_status_header(404);
+		}
+
 		$view = 'html/error_404';
 		$this->render->activeHeader = TRUE;
 		$this->render->titlePage = lang('ERROR_PAGE_404');
-		$this->views = ['errors/'.$view];
+		$this->views = ['errors/' . $view];
 		$this->loadView($view);
 	}
 }
