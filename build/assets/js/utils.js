5eae214f8945292f8d09117169018fd2a75c39c0
Enrique Peñaloza <epenaloza@novopayment.com>
2023-09-29 14:52:38 -0400

    Formato de archivo

--- build/assets/js/utils.js
+++ build/assets/js/utils.js
@@ -1,18 +1,18 @@
 export const appLoader = $('#loader').html();
 
 export const toggleDisableActions = function (disable) {
-	$('button, select,textarea, input:not([type=hidden])').not('[ignore-el]').prop('disabled', disable);
+  $('button, select, textarea, input:not([type=hidden])').not('[ignore-el]').prop('disabled', disable);
 };
 
 export const takeFormData = function (form) {
-	let dataForm = {};
-	form.find('input, select, textarea').each(function (index, element) {
-		dataForm[$(element).attr('name')] = $(element).val();
-	});
+  let dataForm = {};
+  form.find('input, select, textarea').each(function (index, element) {
+    dataForm[$(element).attr('name')] = $(element).val();
+  });
 
-	return dataForm;
+  return dataForm;
 };
 
 export const spinerLoader = function (show) {
-	show ? $('.cover-spin').show(0) : $('.cover-spin').hide();
+  show ? $('.cover-spin').show(0) : $('.cover-spin').hide();
 };
