cae9bf82a1caed5f6d1ab8ae85737872e81dd1d6
Enrique Peñaloza <epenaloza@novopayment.com>
2023-10-06 19:25:46 -0400

    Habilita extención de archivos para dólares

--- build/application/language/global/settings_us_lang.php
+++ build/application/language/global/settings_us_lang.php
@@ -1,7 +1,7 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-$lang['SETT_FILES_EXTENSION'] = 'txt';
+$lang['SETT_FILES_EXTENSION'] = 'txt|xls|xlsx';
 $lang['SETT_ISSUED_MONTHLY'] = 'OFF';
 $lang['SETT_MENU_CIRCLE'] = 'ON';
 $lang['SETT_SIGNIN_IMG'] = 'ON';
