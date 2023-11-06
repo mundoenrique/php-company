0b47ba31de4195f188bbaeef7f10a3670a78a5bb
Enrique Peñaloza <epenaloza@novopayment.com>
2023-11-06 09:24:53 -0400

    Formato de archivo

--- build/application/views/reports/categoryExpense_content-core.php
+++ build/application/views/reports/categoryExpense_content-core.php
@@ -1,24 +1,24 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
 <h1 class="primary h3 regular inline"><?= lang('GEN_MENU_REP_CATEGORY_EXPENSE'); ?></h1>
 <span class="ml-2 regular tertiary"><?= $productName ?></span>
 <div class="mb-2 flex items-center">
-	<div class="flex tertiary">
-		<nav class="main-nav nav-inferior">
-			<ul class="mb-0 h6 light tertiary list-style-none list-inline">
-				<li class="inline"><a class="tertiary big-modal" href="<?= base_url(lang('SETT_LINK_ENTERPRISES')) ?>"><?= lang('GEN_MENU_ENTERPRISE'); ?></a></li> /
-				<li class="inline"><a class="tertiary big-modal" href="<?= base_url(lang('SETT_LINK_PRODUCTS')) ?>"><?= lang('GEN_PRODUCTS'); ?></a></li> /
-				<li class="inline"><a class="tertiary big-modal" href="<?= base_url(lang('SETT_LINK_PRODUCT_DETAIL')) ?>"><?= lang('GEN_PRODUCTS_DETAIL_TITLE'); ?></a></li> /
-				<li class="inline"><a class="tertiary not-pointer" href="javascript:"><?= lang('GEN_MENU_REPORTS'); ?></a></li>
-			</ul>
-		</nav>
-	</div>
+  <div class="flex tertiary">
+    <nav class="main-nav nav-inferior">
+      <ul class="mb-0 h6 light tertiary list-style-none list-inline">
+        <li class="inline"><a class="tertiary big-modal" href="<?= base_url(lang('SETT_LINK_ENTERPRISES')) ?>"><?= lang('GEN_MENU_ENTERPRISE'); ?></a></li> /
+        <li class="inline"><a class="tertiary big-modal" href="<?= base_url(lang('SETT_LINK_PRODUCTS')) ?>"><?= lang('GEN_PRODUCTS'); ?></a></li> /
+        <li class="inline"><a class="tertiary big-modal" href="<?= base_url(lang('SETT_LINK_PRODUCT_DETAIL')) ?>"><?= lang('GEN_PRODUCTS_DETAIL_TITLE'); ?></a></li> /
+        <li class="inline"><a class="tertiary not-pointer" href="<?= lang('SETT_NO_LINK') ?>"><?= lang('GEN_MENU_REPORTS'); ?></a></li>
+      </ul>
+    </nav>
+  </div>
 </div>
 <div class="flex mt-1 bg-color flex-nowrap justify-between">
-	<div id="pre-loader" class="mt-2 mx-auto">
-		<span class="spinner-border spinner-border-lg" role="status" aria-hidden="true"></span>
-	</div>
-	<div class="w-100 hide-out hide">
-		<div class="flex flex-auto flex-column <?= $widget ? '' : 'max-width-6'; ?>">
+  <div id="pre-loader" class="mt-2 mx-auto">
+    <span class="spinner-border spinner-border-lg" role="status" aria-hidden="true"></span>
+  </div>
+  <div class="w-100 hide-out hide">
+    <div class="flex flex-auto flex-column <?= $widget ? '' : 'max-width-6'; ?>">
 
 
 
@@ -26,9 +26,9 @@
 
 
 
-		</div>
-	</div>
-	<?php if($widget): ?>
-	<?php $this->load->view('widget/widget_enterprise-product_content-core', $widget) ?>
-	<?php endif; ?>
+    </div>
+  </div>
+  <?php if ($widget) : ?>
+    <?php $this->load->view('widget/widget_enterprise-product_content-core', $widget) ?>
+  <?php endif; ?>
 </div>
