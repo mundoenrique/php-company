e56ea52c2a4516b830923b7d2a516512fb371e6e
Enrique Peñaloza <epenaloza@novopayment.com>
2023-11-15 14:47:03 -0400

    Formato de archivo

--- build/application/helpers/novo_cryptography_helper.php
+++ build/application/helpers/novo_cryptography_helper.php
@@ -10,52 +10,52 @@ defined('BASEPATH') or exit('No direct script access allowed');
  * @date October 1st, 2022
  */
 if (!function_exists('decryptData')) {
-	/**
-	 * @info decrypt data
-	 * @author epenaloza
-	 * @date October 3rd, 2022
-	 * @param json $requestData data to decrypt
-	 * @return object|array|string data decrypted
-	 */
-	function decryptData($requestData)
-	{
-		$CI = &get_instance();
+  /**
+   * @info decrypt data
+   * @author epenaloza
+   * @date October 3rd, 2022
+   * @param json $requestData data to decrypt
+   * @return object|array|string data decrypted
+   */
+  function decryptData($requestData)
+  {
+    $CI = &get_instance();
 
-		if (ACTIVE_SAFETY) {
-			$req = json_decode(base64_decode($requestData));
-			$requestData = $CI->cryptography->decrypt(base64_decode($req->plot), utf8_encode($req->data));
-		} else {
-			$requestData = $requestData;
-		}
+    if (ACTIVE_SAFETY) {
+      $req = json_decode(base64_decode($requestData));
+      $requestData = $CI->cryptography->decrypt(base64_decode($req->plot), utf8_encode($req->data));
+    } else {
+      $requestData = $requestData;
+    }
 
-		return xss_clean(strip_tags($requestData));
-	}
+    return xss_clean(strip_tags($requestData));
+  }
 }
 
 if (!function_exists('encryptData')) {
-	/**
-	 * @info decrypt data
-	 * @author epenaloza
-	 * @date October 3rd, 2022
-	 * @param object|array|string $responseData data to encrypt
-	 * @return json data encrypted
-	 */
-	function encryptData($responseData)
-	{
-		$CI = &get_instance();
-		$responseData->redirectLink = uriRedirect();
-		$responseData = [
-			'payload' => $responseData
-		];
+  /**
+   * @info decrypt data
+   * @author epenaloza
+   * @date October 3rd, 2022
+   * @param object|array|string $responseData data to encrypt
+   * @return json data encrypted
+   */
+  function encryptData($responseData)
+  {
+    $CI = &get_instance();
+    $responseData->redirectLink = uriRedirect();
+    $responseData = [
+      'payload' => $responseData
+    ];
 
-		if (ACTIVE_SAFETY) {
-			$responseData['payload']->novoName = $CI->security->get_csrf_token_name();
-			$responseData['payload']->novoValue = $CI->security->get_csrf_hash();
-			$responseData['payload'] = base64_encode(
-				json_encode($CI->cryptography->encrypt($responseData['payload']), JSON_UNESCAPED_UNICODE)
-			);
-		}
+    if (ACTIVE_SAFETY) {
+      $responseData['payload']->novoName = $CI->security->get_csrf_token_name();
+      $responseData['payload']->novoValue = $CI->security->get_csrf_hash();
+      $responseData['payload'] = base64_encode(
+        json_encode($CI->cryptography->encrypt($responseData['payload']), JSON_UNESCAPED_UNICODE)
+      );
+    }
 
-		return json_encode($responseData, JSON_UNESCAPED_UNICODE);
-	}
+    return json_encode($responseData, JSON_UNESCAPED_UNICODE);
+  }
 }
