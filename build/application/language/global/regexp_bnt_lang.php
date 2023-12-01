560f67768da33223b1b92ddf3a7fc4609c58ea37
Enrique Peñaloza <epenaloza@novopayment.com>
2023-11-30 20:58:54 -0400

    Validación por tenant

---
+++ build/application/language/global/regexp_bnt_lang.php
@@ -0,0 +1,6 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+$lang['REGEX_KEY_SERVER'] = 'trim|required';
+$lang['REGEX_SERVICE_ID_SERVER'] = 'trim|required';
+$lang['REGEX_CHANNEL_SERVER'] = 'trim|required';
