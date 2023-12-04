914496b1a1bf2a5e7731dfdb4abe5e6207c657da
gerodriguez <gerodriguez@novopayment.com>
2023-12-04 15:11:12 -0400

    Crea archivo reports-lang.php en carpeta: spanish-coop.

---
+++ build/application/language/spanish-coop/reports_lang.php
@@ -0,0 +1,4 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+$lang['REPORTS_ID_FISCAL'] = "Documento de Identidad. (Opcional)";
+$lang['REPORTS_ID_FISCAL_INPUT'] = "Ingresa documento de identidad.";
