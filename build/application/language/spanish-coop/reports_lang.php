6ae1930217668e6c5d5a1e503ca30702603808be
gerodriguez <gerodriguez@novopayment.com>
2023-12-12 08:26:46 -0400

    Agrega variable de lenguaje en: reports_lang (coop).

--- build/application/language/spanish-coop/reports_lang.php
+++ build/application/language/spanish-coop/reports_lang.php
@@ -2,3 +2,5 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 $lang['REPORTS_ID_FISCAL'] = "Documento de Identidad. (Opcional)";
 $lang['REPORTS_ID_FISCAL_INPUT'] = "Ingresa documento de identidad.";
+//CLOSING BALANCE
+$lang['REPORTS_TABLE_DNI'] = "Documento de Identidad";
