0027ae59f23507f85f63b4c588a5239d6e5b6f97
Enrique Peñaloza <epenaloza@novopayment.com>
2024-02-27 08:56:25 -0400

    Expresiones regulares y mensajes

--- build/application/language/spanish-bp/validate_lang.php
+++ build/application/language/spanish-bp/validate_lang.php
@@ -1,5 +1,6 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 //VALIDATE FORMS
 $lang['VALIDATE_EXAMPLE_ID_FISCAL'] = '. Ejm: 0000000000000';
+$lang['VALIDATE_ID_NUMBER'] = 'Indica un número de identificación válido';
