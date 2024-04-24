7054c98b6545996345e7e04fbfb84273b16501d1
Enrique Peñaloza <epenaloza@novopayment.com>
2024-04-24 13:15:03 -0400

    Incluye estilos para voygo

--- build/application/config/config-vg.php
+++ build/application/config/config-vg.php
@@ -2,6 +2,7 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 
 $config['customer'] = 'Usd';
+$config['customer_style'] = CUSTOMER_URI;
 $config['customer_files'] = CUSTOMER_URI;
 $config['language'] = BASE_LANGUAGE . '-' . CUSTOMER_URI;
 $config['channel'] = 'voygoBusiness';
