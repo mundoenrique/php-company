e55a2012d87fd4d177d21a388a85f3d29680dcde
Enrique Peñaloza <epenaloza@novopayment.com>
2024-04-25 10:39:09 -0400

    Archivos de gestión de lotes voygo

---
+++ build/application/language/spanish-vg/tools_lang.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+$lang['TOOLS_FILES_DOWNLOAD'] = [
+  'Manuales' => [
+    ['manual_de_usuario_voygo_admin', 'pdf', 'Manual de usuario voygo admin', 'download'],
+  ],
+  'Archivos de gestión' => [
+    ['gestion-de-lotes', 'rar', 'Archivos lotes operativos', 'download']
+  ]
+];
